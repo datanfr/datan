7c787182fb9c74863dd35af44b2e1b658bd67962
awenig <awenigmar@gmail.com>
2021-02-22 22:08:35 +0100

    remove microsoft clarity from header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -28,15 +28,6 @@
     "readmoreLink": "/cookiespolicy" /* Change the default readmore link */
     });
     </script>
-    <!-- Microsoft Clarity -->
-    <!--
-    <script type="text/javascript">
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "44uteftes6");
-    </script> -->
     <meta charset="utf-8">
     <meta name="robots" content="index,follow">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
