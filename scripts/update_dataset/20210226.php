25ae8dde2b5d1aad7e7fd19351fce635025ac5ba
Rémi Mikel <remikel.pro@gmail.com>
2021-02-26 21:09:47 -0600

    move regions and old regions in departement, insee

--- scripts/update_dataset/20210226.php
+++ scripts/update_dataset/20210226.php
@@ -12,4 +12,19 @@
   $bdd->query('DROP TABLE IF EXISTS class_groups_majorite;');
   $bdd->query('DROP TABLE IF EXISTS class_groups_participation;');
 
+  try {
+    $bdd->query('ALTER TABLE `departement` ADD `region` VARCHAR(255) NULL DEFAULT NULL AFTER `slug`;');
+    $bdd->query('Update departement Set region= (Select region_name from departements_regions where departements_regions.num_dep=departement.departement_code)');
+    $bdd->query('DROP TABLE IF EXISTS departements_regions;');
+  }catch(Exception $e){
+    echo 'departement_regions déjà migrées, enfin vérifie ta base quand même';
+  }
+
+  try {
+    $bdd->query('ALTER TABLE `insee` ADD `new_region` INT;');
+    $bdd->query('Update insee Set region=(Select new_code from regions_old_new where regions_old_new.former_code=insee.region))');
+    $bdd->query('DROP TABLE IF EXISTS regions_old_new;');
+  }catch(Exception $e){
+    echo 'regions_old_new déjà migrées, enfin vérifie ta base quand même';
+  }
 ?>
