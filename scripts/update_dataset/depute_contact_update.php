dc71f4f9e604e018d703e580542da5c7747909c3
awenig <awenigmar@gmail.com>
2021-03-03 23:43:08 +0100

    fix issue with create table query in prod

--- scripts/update_dataset/depute_contact_update.php
+++ scripts/update_dataset/depute_contact_update.php
@@ -3,6 +3,6 @@
 
   $bdd->query('DROP TABLE IF EXISTS deputes_contacts;');
   $bdd->query('DROP TABLE IF EXISTS deputes_contacts_cleaned;');
-  $bdd->query('CREATE TABLE `datan`.`deputes_contacts` ( `mpId` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `website` VARCHAR(255) NULL DEFAULT NULL , `mailAn` VARCHAR(255) NULL DEFAULT NULL , `mailPerso` VARCHAR(255) NULL DEFAULT NULL , `twitter` VARCHAR(255) NULL DEFAULT NULL , `facebook` VARCHAR(255) NULL DEFAULT NULL , `dateMaj` DATE NOT NULL , PRIMARY KEY (`mpId`)) ENGINE = InnoDB;');
+  $bdd->query('CREATE TABLE `deputes_contacts` ( `mpId` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , `website` VARCHAR(255) NULL DEFAULT NULL , `mailAn` VARCHAR(255) NULL DEFAULT NULL , `mailPerso` VARCHAR(255) NULL DEFAULT NULL , `twitter` VARCHAR(255) NULL DEFAULT NULL , `facebook` VARCHAR(255) NULL DEFAULT NULL , `dateMaj` DATE NOT NULL , PRIMARY KEY (`mpId`)) ENGINE = InnoDB;');
 
 ?>
