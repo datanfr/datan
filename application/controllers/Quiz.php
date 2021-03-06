1931f764314a5445b7a41ee59481dadf550d608b
Rémi Mikel <remikel.pro@gmail.com>
2021-03-06 10:31:29 -0600

    start getting rank

--- application/controllers/Quiz.php
+++ application/controllers/Quiz.php
@@ -44,7 +44,69 @@
     public function result(){
       $user_type = $this->session->userdata('type');
       $data['user'] = $user_type;
-      var_dump($this->input->post());die;
+      $votes = $this->votes_model->get_votes_result_by_depute(array_keys($this->input->post()));
+      $groupes = array();
+      $deputes = array();
+      foreach($votes as $vote){
+        foreach($this->input->post() as $post){
+          // if (!isset($groupes[$vote['uid']])){
+          //   $groupes[$vote['uid']] = array();
+          //   $groupes[$vote['uid']]['score'] = 0;
+          //   $groupes[$vote['uid']]['name'] = $vote['libelle'];
+          //   $groupes[$vote['uid']]['totalVote'] = 0;
+          //   $groupes[$vote['uid']]['pour'] = 0;
+          //   $groupes[$vote['uid']]['contre'] = 0;
+          //   $groupes[$vote['uid']]['abstention'] = 0;
+          // }
+          // $groupes[$vote['uid']]['totalVote'] += 1;
+          // $groupes[$vote['uid']]['pour'] += $vote['vote'] == "1" ? 1 :0;
+          // $groupes[$vote['uid']]['contre'] += $vote['vote'] == "-1" ? 1 :0;
+          // $groupes[$vote['uid']]['abstention'] += $vote['vote'] === "0" ? 1 :0;
+
+          // if ($post['choice'] == $vote['vote']){
+          //     $groupes[$vote['uid']]['score'] += $post["weigth"] * 2;
+          // }
+          // else if($vote['vote'] === "0"){
+          //   $groupes[$vote['uid']]['score'] += $post["weigth"];
+          // }
+          // else {
+          //   $groupes[$vote['uid']]['score'] -= $post["weigth"];
+          // }
+          if (!isset($deputes[$vote['mpId']])){
+            $deputes[$vote['mpId']] = array();
+            $deputes[$vote['mpId']]['score'] = 0;
+            $deputes[$vote['mpId']]['name'] = $vote['nameFirst'] . ' '. $vote['nameLast'];
+            $deputes[$vote['mpId']]['totalVote'] = 0;
+            $deputes[$vote['mpId']]['pour'] = 0;
+            $deputes[$vote['mpId']]['contre'] = 0;
+            $deputes[$vote['mpId']]['abstention'] = 0;
+          }
+          if ($vote['nameLast'] == 'Ferrand'){
+            echo '<pre>', var_dump($vote), '</pre>';
+          }
+          $deputes[$vote['mpId']]['totalVote'] += 1;
+          $deputes[$vote['mpId']]['pour'] += $vote['vote'] == "1" ? 1 :0;
+          $deputes[$vote['mpId']]['contre'] += $vote['vote'] == "-1" ? 1 :0;
+          $deputes[$vote['mpId']]['abstention'] += $vote['vote'] === "0" ? 1 :0;
+
+          if ($post['choice'] == $vote['vote']){
+              $deputes[$vote['mpId']]['score'] += $post["weigth"] * 2;
+          }
+          else if($vote['vote'] === "0"){
+            $deputes[$vote['mpId']]['score'] += $post["weigth"];
+          }
+          else {
+            $deputes[$vote['mpId']]['score'] -= $post["weigth"];
+          }
+        }
+      }
+      // foreach($groupes as $key => $groupe){
+      //   $groupes[$key]['averageScore'] = $groupes[$key]['score'] / $groupes[$key]['totalVote']; 
+      // }
+      usort($deputes, function($a, $b){
+        return $a > $b;
+      });
+      echo '<pre>', var_dump($deputes), '</pre>';die('');
       // Breadcrumb
       $data['breadcrumb'] = array(
         array(
