14085a7818628448559ab95015a6f3e84ba9b2cf
awenig <awenigmar@gmail.com>
2021-03-08 10:59:19 +0100

    obfuscation: create new draft function

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -51,4 +51,29 @@
     $url = "sdfghj".str_rot13($x);
     return $url;
   }
+
+  function text_url_obfuscation($text){
+    $dom = new DOMDocument();
+    $dom->encoding = 'utf-8';
+    $dom->loadHTML( utf8_decode($text) );
+    $tags = $dom->getElementsByTagName('a');
+    for ($i = $tags->length - 1; $i > -1 ; $i--) {
+      $tag = $tags->item($i);
+      //print_r($tag);
+
+      if ($tag->getAttribute('target') == '_blank') {
+        
+        echo $tag->getAttribute('href');
+        $replacement = $dom->createTextNode($tag->nodeValue);
+        $tag->parentNode->replaceChild($replacement, $tag);
+
+      }
+
+      $dom->saveHTML();
+
+    }
+
+
+  }
+
 ?>
