1d27d6b7cb88c7d83f32de1a51739d52ba8ea9ff
Rémi Mikel <remikel.pro@gmail.com>
2021-03-11 14:26:15 -0600

    refacto grunt and assets management

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,47 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   var date = new Date();
-  var today = date.getFullYear() + '' + (date.getMonth()+1) + '' + date.getDate();
+  var today = date.getFullYear() + '' + (date.getMonth() + 1) + '' + date.getDate();
 
 
   grunt.initConfig({
+    // First compile main.scss to assets/css/main.css and bootstrap.scss to assets/css/bootstrap.css
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: false,
+          sourcemap: false
+        },
+        files: {
+          'assets/css/main.css': [
+            'main.scss'
+          ],
+          'assets/css/bootstrap.css': [
+            'bootstrap.scss'
+          ]
+        }
+      }
+    },
+    // Purify only bootstrap
     purifycss: {
       options: {
         minify: true
       },
       target: {
         src: ['application/views/*/*.php'],
-        css: ['assets/css/main.css'],
-        dest: 'assets/css/main_' + today + '.css'
+        css: ['assets/css/bootstrap.css'],
+        dest: 'assets/css/bootstrap.css'
+      }
+    },
+    // Concatenate bootstrap and main together to assets/css/main.css
+    concat:{
+      css:{
+        src: [
+          'assets/css/bootstrap.css',
+          'assets/css/main.css'
+        ],
+        dest: 'assets/css/main.css'
       }
     },
     critical: {
@@ -64,29 +93,37 @@ module.exports = function(grunt) {
         // The destination file
         dest: 'assets/css/critical/groupe_individual.css'
       },
+    },
+    uglify: {
+      options: {
+        compress: true
       },
-      uglify: {
-        options: {
-          compress: true
-        },
-        datatables: {
-          src: [
-            'assets/js/datatable/jquery.dataTables.min.js',
-            'assets/js/datatable/dataTables.bootstrap4.min.js',
-            'assets/js/datatable/dataTables.responsive.min',
-            'assets/js/datatable/data-table-datan.js'
-          ],
-          dest: 'assets/js/' + today + '_datatable-datan.min.js'
-        }
+      datatables: {
+        src: [
+          'assets/js/datatable/jquery.dataTables.min.js',
+          'assets/js/datatable/dataTables.bootstrap4.min.js',
+          'assets/js/datatable/dataTables.responsive.min',
+          'assets/js/datatable/data-table-datan.js'
+        ],
+        dest: 'assets/js/datatable-datan.min.js'
+      },
+      main: {
+        src: [
+          'assets/js/main.js'
+        ],
+        dest: 'assets/js/main.min.js'
       }
-    });
+    }
+  });
 
-    // Load the plugins
-    grunt.loadNpmTasks('grunt-critical');
-    grunt.loadNpmTasks('grunt-purifycss');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-critical');
+  grunt.loadNpmTasks('grunt-purifycss');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-    // Default tasks.
-    grunt.registerTask('default', ['purifycss', 'critical', 'uglify']);
+  // Default tasks. Mind the order
+  grunt.registerTask('default', ['sass', 'purifycss', 'concat', 'critical', 'uglify']);
 
 };
