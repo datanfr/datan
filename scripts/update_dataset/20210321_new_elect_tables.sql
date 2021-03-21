9a0e6adf4b6d636203cb264dbb52e6bb7f2e16a9
awenig <awenigmar@gmail.com>
2021-03-21 14:22:04 +0100

    new table region SQL

--- scripts/update_dataset/20210321_new_elect_tables.sql
+++ scripts/update_dataset/20210321_new_elect_tables.sql
@@ -72,6 +72,45 @@ INSERT INTO `elect_libelle` (`id`, `libelle`, `libelleAbrev`, `dateYear`, `dateF
 (1, 'Élections régionales', 'Régionales', 2021, '2021-06-13', '2021-06-20');
 COMMIT;
 
+--
+-- Structure de la table `regions`
+--
+
+DROP TABLE IF EXISTS `regions`;
+CREATE TABLE IF NOT EXISTS `regions` (
+  `reg` varchar(3) DEFAULT NULL,
+  `cheflieu` varchar(8) DEFAULT NULL,
+  `tncc` varchar(4) DEFAULT NULL,
+  `ncc` varchar(26) DEFAULT NULL,
+  `nccner` varchar(26) DEFAULT NULL,
+  `libelle` varchar(26) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `regions`
+--
+
+INSERT INTO `regions` (`reg`, `cheflieu`, `tncc`, `ncc`, `nccner`, `libelle`) VALUES
+('01', '97105', '3', 'GUADELOUPE', 'Guadeloupe', 'Guadeloupe'),
+('02', '97209', '3', 'MARTINIQUE', 'Martinique', 'Martinique'),
+('03', '97302', '3', 'GUYANE', 'Guyane', 'Guyane'),
+('04', '97411', '0', 'LA REUNION', 'La Réunion', 'La Réunion'),
+('06', '97608', '0', 'MAYOTTE', 'Mayotte', 'Mayotte'),
+('11', '75056', '1', 'ILE DE FRANCE', 'Île-de-France', 'Île-de-France'),
+('24', '45234', '2', 'CENTRE VAL DE LOIRE', 'Centre-Val de Loire', 'Centre-Val de Loire'),
+('27', '21231', '0', 'BOURGOGNE FRANCHE COMTE', 'Bourgogne-Franche-Comté', 'Bourgogne-Franche-Comté'),
+('28', '76540', '0', 'NORMANDIE', 'Normandie', 'Normandie'),
+('32', '59350', '4', 'HAUTS DE FRANCE', 'Hauts-de-France', 'Hauts-de-France'),
+('44', '67482', '2', 'GRAND EST', 'Grand Est', 'Grand Est'),
+('52', '44109', '4', 'PAYS DE LA LOIRE', 'Pays de la Loire', 'Pays de la Loire'),
+('53', '35238', '0', 'BRETAGNE', 'Bretagne', 'Bretagne'),
+('75', '33063', '3', 'NOUVELLE AQUITAINE', 'Nouvelle-Aquitaine', 'Nouvelle-Aquitaine'),
+('76', '31555', '1', 'OCCITANIE', 'Occitanie', 'Occitanie'),
+('84', '69123', '1', 'AUVERGNE RHONE ALPES', 'Auvergne-Rhône-Alpes', 'Auvergne-Rhône-Alpes'),
+('93', '13055', '0', 'PROVENCE ALPES COTE D AZUR', 'Provence-Alpes-Côte d\'Azur', 'Provence-Alpes-Côte d\'Azur'),
+('94', '2A004', '0', 'CORSE', 'Corse', 'Corse');
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
