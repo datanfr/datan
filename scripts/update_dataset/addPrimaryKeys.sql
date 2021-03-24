51e3149fa2d5df4223938e8727ed8797f7a37cd2
Rémi Mikel <remikel.pro@gmail.com>
2021-03-23 18:46:12 -0600

    add button to update primary keys

--- scripts/update_dataset/addPrimaryKeys.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `deputes` ADD PRIMARY KEY(`mpId`);
-ALTER TABLE `mandat_groupe` ADD PRIMARY KEY(`mandatId`);
-ALTER TABLE `mandat_principal` ADD PRIMARY KEY(`mandatId`);
-ALTER TABLE `mandat_secondaire` ADD PRIMARY KEY(`mandatId`);
-ALTER TABLE `deputes_all` DROP `id`;
-ALTER TABLE `deputes_all` ADD PRIMARY KEY(`mpId`);
-ALTER TABLE `legislature` DROP `id`;
-ALTER TABLE `legislature` ADD PRIMARY KEY(`legislatureNumber`);
