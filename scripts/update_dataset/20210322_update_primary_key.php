e60d109ca27fec95bb1baf22fe9967dbf82701d1
awenig <awenigmar@gmail.com>
2021-03-25 10:13:21 +0100

    add primary key on organes

--- scripts/update_dataset/20210322_update_primary_key.php
+++ scripts/update_dataset/20210322_update_primary_key.php
@@ -5,6 +5,7 @@
     ALTER TABLE `mandat_groupe` ADD PRIMARY KEY(`mandatId`);
     ALTER TABLE `mandat_principal` ADD PRIMARY KEY(`mandatId`);
     ALTER TABLE `mandat_secondaire` ADD PRIMARY KEY(`mandatId`);
+    ALTER TABLE `organes` ADD PRIMARY KEY(`uid`);
     ALTER TABLE `deputes_last` DROP `id`;
     ALTER TABLE `deputes_last` ADD PRIMARY KEY(`mpId`);
     ALTER TABLE `deputes_all` DROP `id`;
