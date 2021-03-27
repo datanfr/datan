d4901912faa127b015223e6f845fc944b9588696
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2021-03-27 15:20:59 +0100

    candidate modify view working

--- application/models/Elections_model.php
+++ application/models/Elections_model.php
@@ -15,6 +15,18 @@
       return $query->row_array();
     }
 
+    public function get_candidate_full($mpId, $election){
+      $whereQuery = array(
+        'mpId' => $mpId,
+        'election' => $election
+      );
+
+      $query = $this->db->get_where('candidate_full', $whereQuery, 1);
+
+      return $query->row_array();
+    }
+
+
     public function get_all_candidate($election){
       $whereQuery = array(
         'election' => $election
