1ad6cfda7897f941a251f1f6453c62fcb1eeca36
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2021-03-27 14:53:50 +0100

    Display visibility on list

--- scripts/update_dataset/20210321_new_elect_tables.sql
+++ scripts/update_dataset/20210321_new_elect_tables.sql
@@ -52,7 +52,7 @@ DROP VIEW IF EXISTS `candidate_full`;
 -- --------------------------------------------------------
 -- --------------------------------------------------------
 CREATE VIEW candidate_full AS SELECT 
-  edc.mpId as mpId, edc.id as id, `election`, `district`, `position`, `nuance`, `source`,
+  edc.mpId as mpId, edc.id as id, `election`, `district`, `position`, `nuance`, `source`, `visible`,
   dl.`id` as "depute_id", /*`mpId`,*/ `legislature`, `nameUrl`, `civ`, `nameFirst`, `nameLast`, `age`, `dptSlug`, `departementNom`, `departementCode`, `circo`, `mandatId`, dl.`libelle` as "depute_libelle", dl.`libelleAbrev` as "depute_libelleAbrev", `groupeId`, `groupeMandat`, `couleurAssociee`, `dateFin`, `datePriseFonction`, `causeFin`, `img`, `imgOgp`, `dateMaj`, `libelle_1`, `libelle_2`, `active`,
   el.`id` as "election_id", el.`libelle` as "election_libelle", el.`libelleAbrev` as "election_libelleAbrev", `dateYear`, `dateFirstRound`, `dateSecondRound`
   FROM elect_deputes_candidats edc
