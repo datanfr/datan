db0667e2cc94b03dd9edcaae67d11a8e23ea712b
awenig <awenigmar@gmail.com>
2021-03-31 13:20:28 +0200

    first version page deputes/elections/(:any)

--- application/controllers/Elections.php
+++ application/controllers/Elections.php
@@ -25,11 +25,52 @@
       // Meta
       $data['url'] = $this->meta_model->get_url();
       $data['title_meta'] = "Les élections en France - Candidats et résultats | Datan";
-      $data['description_meta'] = "Retrouvez toutes les informations sur les différentes élections en France (présidentielle, législative, régionales). Découvrez les députés candidats et les résultats";
+      $data['description_meta'] = "Retrouvez toutes les informations sur les différentes élections en France (présidentielle, législative, régionales). Découvrez les députés candidats et les résultats.";
       $data['title'] = "Les élections politiques en France";
       // Load Views
       $this->load->view('templates/header', $data);
       $this->load->view('elections/index', $data);
+      $this->load->view('templates/breadcrumb', $data);
+      $this->load->view('templates/footer', $data);
+    }
+
+    public function individual($slug){
+      $data['election'] = $this->elections_model->get_election($slug);
+
+      if (empty($data['election'])) {
+        show_404();
+      }
+
+      // Data
+      $data['deputes'] = $this->elections_model->get_all_candidate($data['election']['id'], TRUE);
+      $data['districts'] = $this->elections_model->get_all_districts($data['election']['id']);
+
+      // Breadcrumb
+      $data['breadcrumb'] = array(
+        array(
+          "name" => "Datan", "url" => base_url(), "active" => FALSE
+        ),
+        array(
+          "name" => "Élections", "url" => base_url()."elections", "active" => FALSE
+        ),
+        array(
+          "name" => $data['election']['libelleAbrev'] . " " . $data['election']['dateYear'], "url" => base_url()."elections/" . $data['election']['slug'], "active" => TRUE
+        )
+      );
+
+      $data['breadcrumb_json'] = $this->breadcrumb_model->breadcrumb_json($data['breadcrumb']);
+      // Meta
+      $data['url'] = $this->meta_model->get_url();
+      $data['title_meta'] = "Les " . mb_strtolower($data['election']['libelle']) . " de " . $data['election']['dateYear'] . " - Candidats et résultats | Datan";
+      $data['description_meta'] = "Retrouvez toutes les informations sur les " . mb_strtolower($data['election']['libelle']) . " de " . $data['election']['dateYear'] . " en France. Découvrez les députés candidats et les résultats.";
+      $data['title'] = "Les " . mb_strtolower($data['election']['libelle']) . " de " . $data['election']['dateYear'];
+      // JS
+      $data['js_to_load_before_datan'] = array("isotope.pkgd.min");
+      $data['js_to_load']= array("datan/sorting");
+      // Load Views
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/button_up');
+      $this->load->view('elections/candidats', $data);
       $this->load->view('templates/breadcrumb', $data);
       $this->load->view('templates/footer', $data);
     }
