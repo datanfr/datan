21ed2b86dedfb7cd2f3d047590d5b10613f3d2c0
Rémi Mikel <remikel.pro@gmail.com>
2021-03-31 10:52:51 -0600

    add newsletter popup

--- application/views/posts/index_category.php
+++ application/views/posts/index_category.php
@@ -50,29 +50,7 @@
           <?php endforeach; ?>
         </ul>
       </div>
-      <div class="card card-right-posts mt-3 card-social-media-posts">
-        <div class="card-header">
-          Suivez-nous
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            <a href="https://www.facebook.com/datanFR/" class="no-decoration underline" target="_blank">
-              <img src="<?= asset_url() ?>imgs/logos/facebook.png" alt="logo Facebook">Facebook</a>
-          </li>
-          <li class="list-group-item">
-            <a href="https://twitter.com/datanFR" class="no-decoration underline" target="_blank">
-              <img src="<?= asset_url() ?>imgs/logos/twitter.png" alt="logo Twitter">Twitter</a>
-          </li>
-        </ul>
-      </div>
-      <div class="card card-right-posts mt-3" style="">
-        <div class="card-header">
-          Derniers tweets
-        </div>
-        <div class="card-body m-0 p-0">
-          <a class="twitter-timeline" data-lang="fr" data-theme="light" data-tweet-limit="2" data-chrome="nofooter noborders noheader transparent" href="https://twitter.com/datanFR?ref_src=twsrc%5Etfw">Tweets by datanFR</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
+      <?php $this->load->view('posts/partials/follow_us.php') ?>
     </div>
   </div>
 </div>
