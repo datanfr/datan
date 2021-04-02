cc9ea19033a19b9940aa95a3f819dcdabca8ec12
awenig <awenigmar@gmail.com>
2021-04-02 12:09:52 +0200

    update style of edatan/elections/(:any)

--- application/views/elections/candidats.php
+++ application/views/elections/candidats.php
@@ -1,32 +1,45 @@
-<div class="container pg-elections-candidats pb-5">
+<div class="container pg-elections-candidats">
   <div class="row bloc-titre">
     <div class="col-12">
       <h1><?= $title ?></h1>
     </div>
-    <div class="col-md-7 col-lg-7 my-4">
-      <p>Les <?= mb_strtolower($election['libelle']) ?> <?= $election['dateYear'] ?> se dérouleront en deux tours.</p>
-      <p>Le premier tour se tiendra le <?= $election['dateFirstRoundFr'] ?>, tandis que le second tour se déroulera le <?= $election['dateSecondRoundFr'] ?>.</p>
-      <p><b>Attention</b>, au vu de la crise sanitaire de la Covid-19, les dates des <?= mb_strtolower($election['libelle']) ?> sont susceptibles de changer.</p>
-      <p>
-        Découvrez sur cette page les députés candidats aux <?= mb_strtolower($election['libelle']) ?> de <?= $election['dateYear'] ?>.
-        <?php if ($election['candidatsN']): ?>
-          Nous avons jusqu'à présent répertorié <span class="font-weight-bold text-primary"><?= $election['candidatsN'] ?> député<?= $election['candidatsN'] > 1 ? "s" : NULL ?> candidat<?= $election['candidatsN'] > 1 ? "s" : NULL ?></span>.
-          <?php else: ?>
-          Nous avons jusqu'à présent répertorié <span class="font-weight-bold text-primary">aucun député candidat</span>.
-        <?php endif; ?>
-      </p>
-      <p>Un député candidat ne se trouve pas dans la liste ? N'hésitez pas à nous le faire savoir: <a href="mailto:contact@datan.fr">contact@datan.fr</a> !</p>
-      <?php if ($election['id'] == 1): ?>
-        <h2 class="mt-5 my-4">La couleur politique actuelle des conseils régionaux</h2>
+  </div>
+</div>
+<div class="container-fluid pg-elections-candidats infosIndividual py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-lg-7 my-4">
+        <h2>Informations</h2>
+        <p>Les <?= mb_strtolower($election['libelle']) ?> <?= $election['dateYear'] ?> se dérouleront en deux tours.</p>
+        <p>Le premier tour se tiendra le <?= $election['dateFirstRoundFr'] ?>, tandis que le second tour se déroulera le <?= $election['dateSecondRoundFr'] ?>.</p>
+        <p><b>Attention</b>, au vu de la crise sanitaire de la Covid-19, les dates des <?= mb_strtolower($election['libelle']) ?> sont susceptibles de changer.</p>
+        <p>
+          Découvrez sur cette page les députés candidats aux <?= mb_strtolower($election['libelle']) ?> de <?= $election['dateYear'] ?>.
+          <?php if ($election['candidatsN']): ?>
+            Nous avons jusqu'à présent répertorié <span class="font-weight-bold"><?= $election['candidatsN'] ?> député<?= $election['candidatsN'] > 1 ? "s" : NULL ?> candidat<?= $election['candidatsN'] > 1 ? "s" : NULL ?></span>.
+            <?php else: ?>
+            Nous avons jusqu'à présent répertorié <span class="font-weight-bold">aucun député candidat</span>.
+          <?php endif; ?>
+        </p>
+        <p>Un député candidat ne se trouve pas dans la liste ? N'hésitez pas à nous le faire savoir: <a href="mailto:contact@datan.fr">contact@datan.fr</a> !</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container pg-elections-candidats py-5">
+  <div class="row">
+    <?php if ($election['id'] == 1): ?>
+      <div class="col-md-8 col-lg-7">
+        <h2 class="my-4">La couleur politique actuelle des conseils régionaux</h2>
         <p>Les dernières élections régionales ont eu lieu en 2015. Pour retrouver les résultats, <a href="https://www.interieur.gouv.fr/Elections/Les-resultats/Regionales/elecresult__regionales-2015/(path)/regionales-2015/index.html" target="_blank" rel="nofollow noreferrer noopener">cliquez ici</a>.</p>
         <p>Dans les conseils régionaux, les partis politiques qui arrivent en têtes aux élections reçoivent la majorité des sièges et en prennent la présidence. C'est donc la liste arrivée en tête qui se retrouve à la tête de la région.</p>
         <p>Découvrez sur cette carte la couleur politique des différentes régions de 2015 à 2021.</p>
-        
-      <?php endif; ?>
-    </div>
+        <p>CARTE</p>
+      </div>
+    <?php endif; ?>
     <?php if (!empty($electionInfos)): ?>
-      <div class="col-md-5 col-lg-4 offset-lg-1">
-        <div class="mt-4 infos">
+      <div class="col-md-8 col-lg-4 offset-lg-1 mt-5 mt-lg-0">
+        <div class="mt-4 infosGeneral">
           <h2 class="title ml-md-5 ml-3">Mieux comprendre</h2>
           <div class="px-4">
             <?= $electionInfos ?>
@@ -35,7 +48,12 @@
       </div>
     <?php endif; ?>
   </div>
-  <div class="row mt-2">
+  <div class="row my-5">
+    <div class="col-12">
+      <h2>Retrouvez les <?= $election['candidatsN'] ?> députés candidats aux <?= mb_strtolower($election['libelle']) ?> de <?= $election['dateYear'] ?></h2>
+    </div>
+  </div>
+  <div class="row">
     <div class="pb-4 col-lg-3 search-element sticky-top sticky-top-lg">
       <div class="sticky-top sticky-offset">
         <!-- Groupes -->
