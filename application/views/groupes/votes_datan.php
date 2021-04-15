1c797641ae23ffaf14f06d284ccdc2e48ce329ca
awenig <awenigmar@gmail.com>
2021-04-15 10:55:49 +0200

    remove lazy image banner on all pages except home

--- application/views/groupes/votes_datan.php
+++ application/views/groupes/votes_datan.php
@@ -1,4 +1,4 @@
-  <div class="container-fluid bloc-img-deputes async_background" id="container-always-fluid" data-src="<?= asset_url() ?>imgs/cover/hemicycle-front.jpg" data-tablet="<?= asset_url() ?>imgs/cover/hemicycle-front-768.jpg" data-mobile="<?= asset_url() ?>imgs/cover/hemicycle-front-375.jpg">
+  <div class="container-fluid bloc-img-deputes async_background" id="container-always-fluid" style="height: 13em"></div>
   </div>
   <div class="liseret-groupe" style="background-color: <?= $groupe['couleurAssociee'] ?>"></div>
   <div class="container pg-groupe-individual">
