85b5b6fd69ff8a174753322faf5e7d3a9ba17c69
awenig <awenigmar@gmail.com>
2021-04-23 12:10:05 +0200

    change historique on individual mp page

--- application/views/deputes/individual.php
+++ application/views/deputes/individual.php
@@ -735,34 +735,45 @@
             <p>
               <?= $title ?> a été élu<?= $gender['e'] ?> à plusieurs reprises. Au total, <?= $gender['pronom'] ?> a été <?= $gender['depute'] ?> à l'Assemblée nationale pendant <?= $depute['lengthEdited'] ?>.
             </p>
+            <p>
+              <?= $title ?> a été député pendant les législatures suivantes :
+              <?php $i = 1; ?>
+              <?php foreach ($mandatsReversed as $mandat): ?>
+                <?php if ($mandat['legislature']): ?>
+                  <i><?= $mandat['legislature'] ?>ème législature</i><?php if ($i < count($mandats)): ?>,<?php else: ?>.<?php endif; ?>
+                <?php endif; ?>
+                <?php $i++; ?>
+              <?php endforeach; ?>
+            </p>
+            <p>
+              Cette page publie les statistiques de <?= $title ?> pour sa législature la plus récente (<?= $depute['legislature'] ?><sup>ème</sup> législature). Nous publions sur Datan l'historique de tous les mandats depuis la 14<sup>ème</sup> législature.
+            </p>
+            <div class="row">
+              <?php foreach ($mandats as $mandat): ?>
+                <?php if ($mandat['legislature'] >= 14 && $mandat['legislature'] != $depute['legislature']): ?>
+                  <div class="col-12 col-sm-6 mt-2 d-flex justify-content-center">
+                    <a class="btn d-flex align-items-center justify-content-center" href="<?= base_url() ?>deputes/<?= $mandat['dptSlug'] ?>/depute_<?= $mandat['nameUrl'] ?>/legislature-<?= $mandat['legislature'] ?>">
+                      <?= $mandat['legislature'] ?>ème législature
+                    </a>
+                  </div>
+                <?php endif; ?>
+              <?php endforeach; ?>
+            </div>
           <?php else: ?>
             <p>
               <?= $title ?> n'a été élu<?= $gender['e'] ?> <?= $gender['depute'] ?> qu'une fois. Au total, <?= $gender['pronom'] ?> a été <?= $gender['depute'] ?> à l'Assemblée nationale pendant <?= $depute['lengthEdited'] ?>.
             </p>
+            <p>
+              <?= $title ?> a été député pendant les législatures suivantes :
+              <?php $i = 1; ?>
+              <?php foreach ($mandatsReversed as $mandat): ?>
+                <?php if ($mandat['legislature']): ?>
+                  <i><?= $mandat['legislature'] ?>ème législature</i><?php if ($i < count($mandats)): ?>,<?php else: ?>.<?php endif; ?>
+                <?php endif; ?>
+                <?php $i++; ?>
+              <?php endforeach; ?>
             </p>
           <?php endif; ?>
-          <div class="row">
-            <?php foreach ($mandats as $mandat): ?>
-              <?php if ($mandat['legislature'] >= 14): ?>
-                <div class="col-12 col-sm-6 mt-2 d-flex justify-content-center">
-                  <a class="btn d-flex align-items-center justify-content-center" href="<?= base_url() ?>deputes/<?= $mandat['dptSlug'] ?>/depute_<?= $mandat['nameUrl'] ?>/legislature-<?= $mandat['legislature'] ?>">
-                    <?= $mandat['legislature'] ?>ème législature
-                  </a>
-                </div>
-              <?php endif; ?>
-            <?php endforeach; ?>
-          </div>
-          <p class="mt-4">
-            En plus de ces mandats, <?= $title ?> a également été député pendant les législatures suivantes :
-            <?php $i = 1; ?>
-            <?php foreach ($mandats as $mandat): ?>
-              <?php if ($mandat['legislature'] < 14): ?>
-                <i><?= $mandat['legislature'] ?>ème législature</i><?php if ($i < count($mandats)): ?>,<?php else: ?>.<?php endif; ?>
-              <?php endif; ?>
-              <?php $i++; ?>
-            <?php endforeach; ?>
-            Nous n'avons pas de données disponibles sur Datan pour ces législatures.
-          </p>
         </div> <!-- // END BLOC HISTORIQUE MANDAT -->
         <!-- BLOC SOCIAL-MEDIA -->
         <div class="bloc-links p-lg-0 p-md-2 mt-5">
