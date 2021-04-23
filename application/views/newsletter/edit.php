18ee3ed75ab7dea0a17a8576f583a1dafff8f977
awenig <awenigmar@gmail.com>
2021-04-23 11:27:05 +0200

    manage newsletter section

--- application/views/newsletter/edit.php
+++ application/views/newsletter/edit.php
@@ -7,16 +7,17 @@
                 </div>
             </div>
             <div class="row mb-4">
-                <div class="col">
-                    <h2>Abonnement de <?= $newsletter['email'] ?></h2>
-                    <br>
-                    <?= form_open('newsletter/edit/' . $newsletter['email']) ?>
+                <div class="col-12">
+                    <h2 class="mb-3">Abonnement(s) de <?= $newsletter['email'] ?></h2>
+                    <?= form_open('newsletter/edit/' . urlencode($newsletter['email'])) ?>
                     <div class="form-check">
                         <input type="checkbox" class="form-check-input" name="general" id="general" <?= !$newsletter['general'] ?: 'checked' ?>>
                         <label for="general">Inscription à la newsletter principale</label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Mettre à jour </button>
+                    <button type="submit" name="submit" class="btn btn-primary my-3">Mettre à jour</button>
                     <?= form_close() ?>
+                    <h2 class="mt-5">Se désabonner de toutes les newsletters</h2>
+                    <p>Si vous souhaitez vous désabonnez de toutes les newsletters, <a href="<?= base_url() ?>newsletter/delete/<?= urlencode($newsletter['email']) ?>">cliquez ici</a>.</p>
                 </div>
             </div>
         </div>
