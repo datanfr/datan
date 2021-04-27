c3cb5b15447b465aa0291b168690668590ffa807
awenig <awenigmar@gmail.com>
2021-04-27 21:42:43 +0200

    debug19

--- application/views/newsletter/edit_success.php
+++ application/views/newsletter/edit_success.php
@@ -10,7 +10,7 @@
               <div class="col-lg-6 mb-4 mb-lg-0">
                 <p><b>Félicitations !</b> Vous avez mis à jour vos abonnements aux newsletters de Datan.</p>
                 <div class="d-flex justify-content-around my-5">
-                  <a href="<?= base_url() ?>/newsletter/edit/<?= urlencode($newsletter['email']) ?>" class="btn btn-warning">Retour</a>
+                  <a href="<?= base_url() ?>/newsletter/edit/<?= $newsletter['email'] ?>" class="btn btn-warning">Retour</a>
                   <a href="<?= base_url() ?>" class="btn btn-primary">Découvrez Datan !</a>
                 </div>
               </div>
