d2981d09f09d2c026f1579330a0dc9ea978478e0
awenig <awenigmar@gmail.com>
2021-04-27 22:09:15 +0200

    debug30

--- application/models/Newsletter_model_testbis.php
+++ application/models/Newsletter_model_testbis.php
@@ -1,5 +1,5 @@
  <?php
-  class Newsletter_model_test extends CI_Model{
+  class Newsletter_model_testbis extends CI_Model{
     public function __construct(){
       //$this->load->database();
       //$this->load->helper('email_helper');
