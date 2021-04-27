f79df1bcf742cb7237c860628e9b8509210fceff
awenig <awenigmar@gmail.com>
2021-04-27 21:59:11 +0200

    debug22

---
+++ application/controllers/Newsletter_test.php
@@ -0,0 +1,14 @@
+<?php
+  class Newsletter_test extends CI_Controller{
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    // Delete all cache
+    public function edit(){
+      echo "yes";
+    }
+
+  }
+?>
