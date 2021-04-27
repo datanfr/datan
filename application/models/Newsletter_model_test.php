55e8dd1102a963e78efd450a4e0ad59e5caa5f70
awenig <awenigmar@gmail.com>
2021-04-27 22:07:18 +0200

    debug28

--- application/models/Newsletter_model_test.php
+++ application/models/Newsletter_model_test.php
@@ -1,11 +1,32 @@
- <?php
+<?php
   class Newsletter_model_test extends CI_Model{
     public function __construct(){
-      //$this->load->database();
-      //$this->load->helper('email_helper');
-      //$this->load->library('parser');
+      $this->load->database();
     }
 
+    public function get_fields(){
+      $this->db->order_by('name');
+      $query = $this->db->get('fields');
+
+      return $query->result_array();
+    }
+
+    public function get_field($field){
+      $query = $this->db->get_where('fields', array('slug' => $field), 1);
+
+      return $query->row_array();
+    }
+
+    public function get_active_fields(){
+      $query = $this->db->query('
+        SELECT f.id, f.name, f.slug, count(v.title) AS number
+        FROM votes_datan v
+        JOIN fields f ON f.id = v.category
+        WHERE v.state = "published"
+        GROUP BY f.name
+      ');
+      return $query->result_array();
+    }
 
 
   }
