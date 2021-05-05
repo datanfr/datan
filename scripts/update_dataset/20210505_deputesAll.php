4d67c6b834477ae024c3fd49a58a48ceb603f7e3
awenig <awenigmar@gmail.com>
2021-05-05 09:18:10 +0200

    add new famSocPro database

--- scripts/update_dataset/20210505_deputesAll.php
+++ scripts/update_dataset/20210505_deputesAll.php
@@ -7,6 +7,21 @@
     ADD `famSocPro` VARCHAR(255) NULL DEFAULT NULL AFTER `catSocPro`;
     ');
 
-    $bdd->query('ALTER TABLE `deputes`
-      ADD `famSocPro` VARCHAR(255) NULL DEFAULT NULL AFTER `catSocPro`;
-      ');
+  $bdd->query('ALTER TABLE `deputes`
+    ADD `famSocPro` VARCHAR(255) NULL DEFAULT NULL AFTER `catSocPro`;
+    ');
+
+  $bdd->query('DROP TABLE IF EXISTS `famsocpro`');
+  $bdd->query('CREATE TABLE IF NOT EXISTS `famsocpro` (
+    `famille` varchar(255) DEFAULT NULL,
+    `population` varchar(4) DEFAULT NULL
+  )');
+  $bdd->query('INSERT INTO `famsocpro` (`famille`, `population`) VALUES
+  ('Agriculteurs exploitants', '1,4'),
+  ('Artisans, commerçants, chefs d\'entreprise', '6,8'),
+  ('Cadres et professions intellectuelles supérieures', '20,4'),
+  ('Professions intermédiaires', '26,0'),
+  ('Employés', '25,8'),
+  ('Ouvriers', '19,2'),
+  ('Non déterminé', '0,4');
+  ');
