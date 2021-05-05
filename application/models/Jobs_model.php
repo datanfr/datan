b852b04f68e7486c90be0a1c88e196afaa5906b9
awenig <awenigmar@gmail.com>
2021-05-05 10:47:34 +0200

    new graph on stat webpage

--- application/models/Jobs_model.php
+++ application/models/Jobs_model.php
@@ -4,17 +4,45 @@
       $this->load->database();
     }
 
-    public function get_stats_famSocPro($famSocPro, $legislature){
-      $where = array(
-        'legislature' => $legislature,
-        'famSocPro' => $famSocPro
-      );
-      $this->db->select('famSocPro, count(mpId) AS n, round(count(mpId) / 577 * 100, 2) AS pct');
-      $this->db->from('deputes_all');
-      $this->db->where($where);
-      $this->db->group_by('famSocPro');
-      $this->db->limit(1);
-      return $this->db->get()->row_array();
+    public function get_stats_individual($famSocPro, $legislature){
+      $sql = 'SELECT A.*, round(A.n / A.total * 100, 2) AS pct
+        FROM
+        (
+        	SELECT da.famSocPro, count(da.mpId) AS n,
+        	(
+        		SELECT COUNT(*) AS total
+        		FROM deputes_last
+        		WHERE legislature = 15 AND dateFin IS NULL AND famSocPro NOT IN ("", "Autres personnes sans activité professionnelle", "Sans profession déclarée")
+        	) AS total
+        	FROM deputes_all da
+        	WHERE da.famSocPro = ? AND da.legislature = ? AND da.dateFin IS NULL
+        	GROUP BY da.famSocPro
+        ) A
+      ';
+
+      $query = $this->db->query($sql, array($famSocPro, $legislature), 1);
+      return $query->row_array();
+    }
+
+    public function get_stats_all_mp($legislature){
+      $sql = 'SELECT fam.famille, round(A.n / A.total * 100, 2) AS mps, fam.population
+        FROM
+        (
+        	SELECT da.famSocPro, count(da.mpId) AS n,
+        	(
+        		SELECT COUNT(*) AS total
+        		FROM deputes_last
+        		WHERE legislature = ? AND dateFin IS NULL AND famSocPro NOT IN ("", "Autres personnes sans activité professionnelle", "Sans profession déclarée")
+        	) AS total
+        	FROM deputes_all da
+        	WHERE da.famSocPro NOT IN ("", "Autres personnes sans activité professionnelle", "Sans profession déclarée") AND da.legislature = ? AND da.dateFin IS NULL
+        	GROUP BY da.famSocPro
+        ) A
+        RIGHT JOIN famSocPro fam ON A.famSocPro = fam.famille
+      ';
+
+      $query = $this->db->query($sql, array($legislature, $legislature));
+      return $query->result_array();
     }
 
 
