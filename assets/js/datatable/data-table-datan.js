52f117aa4332eefd943ae63dd649212a153e7162
awenig <awenigmar@gmail.com>
2021-05-07 10:21:58 +0200

    fix datatables js issues

--- assets/js/datatable/data-table-datan.js
+++ assets/js/datatable/data-table-datan.js
@@ -217,6 +217,61 @@
       }
     });
 
+    $('#table-stats-origine-sociale').DataTable( {
+      "ordering": false,
+      "info": false,
+      responsive: {
+        details: false
+      },
+      initComplete: function () {
+          this.api().columns().every( function (i) {
+            var column = this;
+            if (screen.width > 767) {
+              if (i == 1 | i == 2 | i == 3) {
+                $(column.header()).append("</br>");
+                var select = $('<select style="margin-top: 12px; margin-bottom: 5px" class="form-control"><option value=""></option></select>')
+                    .appendTo( $(column.header()) )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                    column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' )
+                    } );
+              }
+            }
+        });
+      },
+      language: {
+          processing:     "Traitement en cours...",
+          search:         "Rechercher un député&nbsp;:",
+          lengthMenu:    "Afficher _MENU_ votes",
+          info:           "Affichage des votes _START_ &agrave; _END_ sur _TOTAL_ votes",
+          infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+          infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+          infoPostFix:    "",
+          loadingRecords: "Chargement en cours...",
+          zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+          emptyTable:     "Aucune donnée disponible dans le tableau",
+          paginate: {
+              first:      "Premier",
+              previous:   "Pr&eacute;c&eacute;dent",
+              next:       "Suivant",
+              last:       "Dernier"
+          },
+          aria: {
+              sortAscending:  ": activer pour trier la colonne par ordre croissant",
+              sortDescending: ": activer pour trier la colonne par ordre décroissant"
+          }
+      }
+    } );
+
 	});
 
 })(jQuery);
