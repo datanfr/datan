60b926320488601eaf9de999a7724c0d9f070ca6
remikel <remikel0729@gmail.com>
2021-05-23 12:58:34 +0200

    fix script

--- scripts/update_dataset/20210505_deputesAll.php
+++ scripts/update_dataset/20210505_deputesAll.php
@@ -18,7 +18,7 @@
     `population` varchar(10) DEFAULT NULL,
     PRIMARY KEY (`id`)
   )');
-  $bdd->query('INSERT INTO `famsocpro` (`id`, `famille`, `population`) VALUES
+  $bdd->query("INSERT INTO `famsocpro` (`id`, `famille`, `population`) VALUES
   (1, 'Agriculteurs exploitants', '0.7'),
   (2, 'Artisans, commerçants et chefs d\'entreprise', '3.5'),
   (3, 'Cadres et professions intellectuelles supérieures', '10.6'),
@@ -27,4 +27,4 @@
   (6, 'Ouvriers', '10.9'),
   (7, 'Retraités', '33.4'),
   (8, 'Personnes n’ayant jamais travaillé (ex étudiants)', '12.7')
-  ');
+  ");
