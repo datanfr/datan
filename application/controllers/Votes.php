adb2cd8bfd97a2e52038889411a03a37afd284b2
awenig <awenigmar@gmail.com>
2021-06-05 19:40:47 +0200

    add vercel img on schema

--- application/controllers/Votes.php
+++ application/controllers/Votes.php
@@ -458,6 +458,9 @@
       $controller = $this->router->fetch_class()."/".$this->router->fetch_method();
       if ($voteDatan) {
         $title_ogp = "Vote Assemblée nationale : " . $data['vote']['title'] . " | Datan";
+        $title_og_img = str_replace(' ', '%20', $data['vote']['title']);
+        $date_og_img = str_replace(' ', '%20', $data['vote']['date_edited']);
+        $data['vote']['og_image'] = 'https://og-image-datan.vercel.app/'.$title_og_img.'?voteN='.$data['vote']['voteNumero'].'&legislature='.$data['vote']['legislature'].'&date='.$date_og_img.'&pour='.$data['vote']['pour'].'&abs='.$data['vote']['abstention'].'&sort='.$data['vote']['sortCode'];
       } else {
         $title_ogp = $data['title_meta'];
       }
