f75586d8c76bf1e4cd57ff6439d0930e768ee307
awenig <awenigmar@gmail.com>
2021-06-14 09:56:43 +0200

    create the two db tables

---
+++ scripts/update_dataset/20210614_faq.php
@@ -0,0 +1,37 @@
+<?php
+  include('../bdd-connexion.php');
+
+
+  // Table faq_categories
+
+  $bdd->query('DROP TABLE IF EXISTS `faq_categories`');
+
+  $bdd->query('CREATE TABLE IF NOT EXISTS `faq_categories` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    `slug` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;');
+
+  $bdd->query("INSERT INTO `faq_categories` (`name`, `slug`) VALUES
+    ('Datan', 'datan'),
+    ('Nos statistiques', 'statistiques'),
+    ('Assemblée nationale', 'assemblee-nationale'),
+    ('Députés', 'deputes'),
+    ('Groupes parlementaires', 'groupes'),
+    ('Votes', 'votes')
+  ");
+
+  // Table faq_posts
+
+  $bdd->query("DROP TABLE IF EXISTS `faq_posts`");
+
+  $bdd->query("CREATE TABLE IF NOT EXISTS `faq_posts` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` text NOT NULL,
+    `text` text NOT NULL,
+    `category` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+
+?>
