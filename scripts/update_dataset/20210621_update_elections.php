4474dbf5dc86da36969d264ae4a3bbf1f1e11315
awenig <awenigmar@gmail.com>
2021-06-21 10:26:52 +0200

    update view canndidates_full

--- scripts/update_dataset/20210621_update_elections.php
+++ scripts/update_dataset/20210621_update_elections.php
@@ -6,3 +6,13 @@
   $bdd->query('UPDATE `elect_libelle` SET `candidates` = false WHERE `elect_libelle`.`id` = 2');
 
   $bdd->query('ALTER TABLE `elect_deputes_candidats` ADD `secondRun` BOOLEAN NULL AFTER `visible`, ADD `elected` BOOLEAN NULL AFTER `secondRun`');
+
+  $bdd->query('DROP VIEW IF EXISTS `candidate_full`');
+  $bdd->query('CREATE VIEW candidate_full AS SELECT
+    edc.mpId as mpId, `election`, `district`, `position`, `nuance`, `source`, `visible`, `secondRun`, `elected`,
+    `legislature`, `nameUrl`, `civ`, `nameFirst`, `nameLast`, `age`, `dptSlug`, `departementNom`, `departementCode`, `circo`, `mandatId`, dl.`libelle` as "depute_libelle", dl.`libelleAbrev` as "depute_libelleAbrev", `groupeId`, `groupeMandat`, `couleurAssociee`, `dateFin`, `datePriseFonction`, `causeFin`, `img`, `imgOgp`, `dateMaj`, `libelle_1`, `libelle_2`, `active`,
+    el.`id` as "election_id", el.`libelle` as "election_libelle", el.`libelleAbrev` as "election_libelleAbrev", `dateYear`, `dateFirstRound`, `dateSecondRound`, edc.`modified_at`
+    FROM elect_deputes_candidats edc
+    LEFT JOIN deputes_last dl ON edc.mpId = dl.mpId
+    LEFT JOIN elect_libelle el ON edc.election = el.id;
+  ');
