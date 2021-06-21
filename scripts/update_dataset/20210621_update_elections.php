fa21a598a20ecc6eb524c70669008162041131f9
awenig <awenigmar@gmail.com>
2021-06-21 10:20:18 +0200

    update datatable new fields

--- scripts/update_dataset/20210621_update_elections.php
+++ scripts/update_dataset/20210621_update_elections.php
@@ -4,3 +4,5 @@
   $bdd->query('ALTER TABLE `elect_libelle` ADD `candidates` BOOLEAN NULL DEFAULT NULL AFTER `dateSecondRound`;');
   $bdd->query('UPDATE `elect_libelle` SET `candidates` = true WHERE `elect_libelle`.`id` = 1');
   $bdd->query('UPDATE `elect_libelle` SET `candidates` = false WHERE `elect_libelle`.`id` = 2');
+
+  $bdd->query('ALTER TABLE `elect_deputes_candidats` ADD `secondRun` BOOLEAN NULL AFTER `visible`, ADD `elected` BOOLEAN NULL AFTER `secondRun`');
