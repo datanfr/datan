fa0b4a36711eba4cb114301db9cd52d5d9cf61bd
awenig <awenigmar@gmail.com>
2021-06-22 09:54:04 +0200

    remove unused page

--- application/views/pages/test.php
+++
@@ -1,7 +0,0 @@
-<div class="" style="padding: 100px; height: 300px">
-  <div class="your-class">
-    <div style="background-color: blue; margin: 20px">your content</div>
-    <div style="background-color: blue; margin: 20px">your content</div>
-    <div style="background-color: blue; margin: 20px">your content</div>
-  </div>
-</div>
