b280cf27b4492e52bfcb8e2ba17e9b4fa613bf36
awenig <awenigmar@gmail.com>
2021-06-23 14:04:40 +0200

    bug filter isotop

--- application/views/elections/candidats.php
+++ application/views/elections/candidats.php
@@ -132,8 +132,10 @@
       <div class="col-lg-9 col-md-12">
         <div class="row mt-2 sorting">
           <?php foreach ($deputes as $depute): ?>
-            <div class="col-md-6 col-xl-4 d-flex justify-content-center sorting-item <?= strtolower($depute["districtId"]) ?>">
-              <?php $this->load->view('deputes/partials/card_home.php', array('depute' => $depute, 'tag' => 'h3')) ?>
+            <div class="col-md-6 col-xl-4 sorting-item <?= strtolower($depute["districtId"]) ?>">
+              <div class="d-flex justify-content-center">
+                <?php $this->load->view('deputes/partials/card_home.php', array('depute' => $depute, 'tag' => 'h3')) ?>
+              </div>
             </div>
           <?php endforeach; ?>
         </div>
