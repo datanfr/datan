618704572b85b4a9934bc2b29fa059f9859f8464
awenig <awenigmar@gmail.com>
2021-07-06 09:35:13 +0200

    add breadcrumb on FAQ

--- application/controllers/Faq.php
+++ application/controllers/Faq.php
@@ -15,6 +15,16 @@
       $data['url'] = $this->meta_model->get_url();
       $data['title_meta'] = "Foire aux questions - Assemblée nationale | Datan";
       $data['description_meta'] = "Comment les députés sont-ils élus ? Quels sont leur rôle au parlement ? Découvrez la foire aux questions (FAQ) de Datan.";
+      // Breadcrumb
+      $data['breadcrumb'] = array(
+        array(
+          "name" => "Datan", "url" => base_url(), "active" => FALSE
+        ),
+        array(
+          "name" => "Foire aux questions", "url" => base_url()."faq", "active" => TRUE
+        )
+      );
+      $data['breadcrumb_json'] = $this->breadcrumb_model->breadcrumb_json($data['breadcrumb']);
       // Open Graph
       $data['ogp'] = $this->meta_model->get_ogp('home', $data['title_meta'], $data['description_meta'], $data['url'], $data);
       // FAQ schema
@@ -25,6 +35,7 @@
       $this->load->view('templates/header', $data);
       $this->load->view('templates/button_up');
       $this->load->view('faq/index', $data);
+      $this->load->view('templates/breadcrumb', $data);
       $this->load->view('templates/footer', $data);
     }
 
