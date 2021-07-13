f76daa2199aa4e545b4b99c32e6eb6c5c0b99357
remikel <remikel0729@gmail.com>
2021-07-13 11:43:15 +0200

    delete useless js

--- application/views/templates/footer_no_navbar.php
+++ application/views/templates/footer_no_navbar.php
@@ -2,9 +2,6 @@
   </main>
     <script src="<?= asset_url(); ?>js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?= asset_url() ?>js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?= asset_url() ?>js/datan_js.js"></script>
-    <script src="<?= asset_url(); ?>js/isotope.pkgd.js"></script>
-    <script src="<?= asset_url(); ?>js/sort_mps.js"></script>
 
   </body>
 </html>
