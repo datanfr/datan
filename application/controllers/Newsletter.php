cf9b87c5dd6a13488476d1b11643705375ea0231
awenig <awenigmar@gmail.com>
2021-08-21 01:22:25 +0200

    remove delete section

--- application/controllers/Newsletter.php
+++ application/controllers/Newsletter.php
@@ -95,29 +95,4 @@ class Newsletter extends CI_Controller
         }
       }
     }
-
-    public function delete($email){
-        $email = ($email);
-        $data['email'] = $email;
-        $data['url'] = $this->meta_model->get_url();
-        $data['title_meta'] = "Désinscription à la newsletter | Datan";
-        $data['title'] = 'Désinscription à la newsletter';
-        $response = getContactId($email);
-        if ($response->success()) {
-          $emailId = $response->getData()[0]["ContactID"];
-          $list = 25834;
-          removeContactlist($emailId, $list);
-        }
-
-        if ($this->newsletter_model->get_by_email($email)){
-            $this->newsletter_model->delete_newsletter($email);
-            $data['message'] = 'Vous ne recevrez plus nos newsletters, c\'est dommage mais n\'hésitez pas à revenir pour connaitre l\'activité de vos députés !';
-        }
-        else {
-            $data['message'] = 'Vous ne semblez pas être actuellement inscrit à la newsletter, si malgré tout vous continuez de recevoir des e-mails de notre part merci de nous contacter.';
-        }
-        $this->load->view('templates/header', $data);
-        $this->load->view('newsletter/delete', $data);
-        $this->load->view('templates/footer', $data);
-    }
 }
