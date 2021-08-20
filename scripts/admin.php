38d0a2db72f62b5fe06f13d509a36055e63eb5f3
awenig <awenigmar@gmail.com>
2021-08-21 00:25:19 +0200

    add new column in newsletter table

--- scripts/admin.php
+++ scripts/admin.php
@@ -43,6 +43,16 @@
               <br>
               <h3 class="mt-3">Delete caching of webpages+SQL queries</h3>
               <a class="btn btn-primary my-3" href="../cache/delete_all" role="button">Delete all caching</a>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Update database : new newsletter (votes)</h5>
+                  <h6 class="card-subtitle text-muted">August 21, 2021</h6>
+                  <p class="card-text">
+                    Add new newsletter (votes)
+                    <a class="btn btn-danger my-3" href="update_dataset/20210821_add_newsletter.php" role="button">UPDATE DATABASE</a>
+                  </p>
+                </div>
+              </div>
               <br>
               <div class="card">
                 <div class="card-body">
