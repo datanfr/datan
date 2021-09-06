457078cbf03ad455ac8432d57f1944e9896af366
awenig <awenigmar@gmail.com>
2021-09-06 18:22:23 +0200

    minor changes : text

--- application/views/newsletterTemplates/templates/header.php
+++ application/views/newsletterTemplates/templates/header.php
@@ -20,7 +20,7 @@
     </mj-style>
   </mj-head>
   <mj-body background-color="#F4F4F4" color="#5e6977" font-family="Open Sans" font-size="13px">
-    <mj-section background-color="transparent" background-repeat="no-repeat" padding-left="0px" padding-right="0px" padding="20px 0px 20px 0px" text-align="center" vertical-align="top">
+    <mj-section background-color="transparent" background-repeat="no-repeat" padding="20px 0px 20px 0px" text-align="center" vertical-align="top">
       <mj-column vertical-align="top">
         <mj-image align="center" alt="Logo Datan" border-radius="px" border="none" container-background-color="#00b794" height="auto" href="https://datan.fr" onUpload="" padding-left="25px" padding-right="25px" padding="10px 25px 10px 25px" src="https://x91ms.mjt.lu/tplimg/x91ms/b/szioh/uqz8.png" target="_blank" title="" vertical-align="top" width="1200px"></mj-image>
       </mj-column>
