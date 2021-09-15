d0aa3de98db9e7f0404863401a6055a8721da176
awenig <awenigmar@gmail.com>
2021-09-15 22:46:24 +0200

    send mechanism in GET parameter

--- application/controllers/Newsletter.php
+++ application/controllers/Newsletter.php
@@ -162,7 +162,9 @@ class Newsletter extends CI_Controller
       $emails = $this->newsletter_model->get_emails("votes");
       foreach ($emails as $email) {
         $title = $data['month'] . " " . $data['year'] . " | Les derniers votes à l'Assemblée nationale";
-      //sendMail($email['email'], $title, $templateHtml = $html, $templateLanguage = TRUE, $templateId = NULL, $variables = NULL);
+        if (isset($_GET["state"]) && $_GET["state"] == "send") {
+          sendMail($email['email'], $title, $templateHtml = $html, $templateLanguage = TRUE, $templateId = NULL, $variables = NULL);
+        }
       }
 
     }
