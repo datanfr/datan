414f5fb3f67eace43f0dee4ca939acc4e079640f
awenig <awenigmar@gmail.com>
2021-09-21 11:32:52 +0200

    Fix MP issue

--- application/controllers/Votes.php
+++ application/controllers/Votes.php
@@ -394,6 +394,7 @@
 
       // Votes - députés
       $data['deputes'] = $this->votes_model->get_vote_deputes($data['vote']['voteNumero'], $legislature);
+      //print_r($data['deputes']);
       // OTHER VOTES
       if ($num != 1) {
         $previous = $num - 1;
