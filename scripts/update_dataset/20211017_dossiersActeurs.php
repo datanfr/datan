fd1e85c36d1bced784a39a58b2a44262d477c1df
awenig <awenigmar@gmail.com>
2021-10-17 23:34:22 +0200

    update create dossiersActeurs table

--- scripts/update_dataset/20211017_dossiersActeurs.php
+++ scripts/update_dataset/20211017_dossiersActeurs.php
@@ -5,7 +5,8 @@
 
   $bdd->query("CREATE TABLE `dossiersActeurs` (
     `id` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-    `acteurType` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-    `acteurRef` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-    `mandatRef` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+    `value` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+    `type` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+    `ref` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+    `mandate` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
   ) ENGINE = MyISAM;");
