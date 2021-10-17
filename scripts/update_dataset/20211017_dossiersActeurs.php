5e53ef27059f15f1aec0fce1f0473f07ba06a294
awenig <awenigmar@gmail.com>
2021-10-18 01:49:45 +0200

    Add two columns to dossiersActeurs table

--- scripts/update_dataset/20211017_dossiersActeurs.php
+++ scripts/update_dataset/20211017_dossiersActeurs.php
@@ -5,8 +5,10 @@
 
   $bdd->query("CREATE TABLE `dossiersActeurs` (
     `id` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+    `legislature` INT(5) NOT NULL ,
     `value` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `type` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `ref` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-    `mandate` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+    `mandate` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+    `dateMaj` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
   ) ENGINE = MyISAM;");
