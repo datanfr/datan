c87fbd69cf0e018387fc6678e1fcfccb9290513d
awenig <awenigmar@gmail.com>
2021-10-18 09:51:42 +0200

    Change name table deputesacteurs --> deputes_acteurs

--- scripts/update_dataset/20211017_dossiersActeurs.php
+++ scripts/update_dataset/20211017_dossiersActeurs.php
@@ -1,14 +1,14 @@
 <?php
   include('../bdd-connexion.php');
 
-  $bdd->query('DROP TABLE IF EXISTS `dossiersActeurs`');
+  $bdd->query('DROP TABLE IF EXISTS `dossiers_acteurs`');
 
-  $bdd->query("CREATE TABLE `dossiersActeurs` (
+  $bdd->query("CREATE TABLE `dossiers_acteurs` (
     `id` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `legislature` INT(5) NOT NULL ,
     `value` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `type` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `ref` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
     `mandate` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-    `dateMaj` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+    `dateMaj` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   ) ENGINE = MyISAM;");
