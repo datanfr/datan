a97904aedf8b1e8980aba2c15d9e167ce2b6ce39
awenig <awenigmar@gmail.com>
2021-10-25 19:45:44 +0200

    Add rapporteur if no author (for proposition de loi ordinaire)

--- application/controllers/Votes.php
+++ application/controllers/Votes.php
@@ -441,11 +441,20 @@
           }
         } elseif (in_array($data['vote']['procedureParlementaireLibelle'], array('Proposition de loi ordinaire', 'Résolution Article 34-1', 'Résolution'))) {
           $data['authorType'] = "mps";
-          $data['author'] = $this->votes_model->get_dossier_mp_authors($data['vote']['dossierId']);
-          if (count($data['author']) == 1) {
-            $data['authorTitle'] = "L'auteur de la proposition de loi";
+          $data['author'] = $this->votes_model->get_dossier_mp_authors($data['vote']['dossierId'], $legislature);
+          if ($data['author']) {
+            if (count($data['author']) == 1) {
+              $data['authorTitle'] = "L'auteur de la proposition de loi";
+            } else {
+              $data['authorTitle'] = "Les auteurs de la proposition de loi";
+            }
           } else {
-            $data['authorTitle'] = "Les auteurs de la proposition de loi";
+            $data['author'] = $this->votes_model->get_dossier_mp_rapporteurs($data['vote']['dossierId'], $legislature);
+            if (count($data['author']) == 1) {
+              $data['authorTitle'] = "Le rapporteur";
+            } else {
+              $data['authorTitle'] = "Les rapporteurs";
+            }
           }
         } elseif (in_array($data['vote']['procedureParlementaireLibelle'], array('Projet de ratification des traités et conventions'))) {
           $data['authorType'] = "mps";
