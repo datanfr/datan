b34df3b99939704843400547425122a16a980e34
awenig <awenigmar@gmail.com>
2021-11-11 09:51:04 +0100

    Reading on vote cards: front-end

--- application/views/votes/partials/votes_carousel.php
+++ application/views/votes/partials/votes_carousel.php
@@ -1,29 +1,34 @@
 <div class="carousel-cards">
-    <?php foreach ($votes as $vote) : ?>
-        <div class="card card-vote">
-            <div class="thumb d-flex align-items-center <?= $vote['sortCode'] ?>">
-                <div class="d-flex align-items-center">
-                    <span><?= mb_strtoupper($vote['sortCode']) ?></span>
-                </div>
-            </div>
-            <div class="card-header d-flex flex-row justify-content-between">
-                <span class="date"><?= months_abbrev($vote['dateScrutinFR']) ?></span>
-            </div>
-            <div class="card-body d-flex align-items-center">
-                <span class="title">
-                    <a href="<?= base_url() ?>votes/legislature-<?= $vote['legislature'] ?>/vote_<?= $vote['voteNumero'] ?>" class="stretched-link no-decoration">
-                        <?= $vote['voteTitre'] ?>
-                    </a>
-                  </span>
-            </div>
-            <div class="card-footer">
-                <span class="field badge badge-primary py-1 px-2"><?= $vote['category_libelle'] ?></span>
-            </div>
+  <?php foreach ($votes as $vote) : ?>
+    <div class="card card-vote">
+      <div class="thumb d-flex align-items-center <?= $vote['sortCode'] ?>">
+        <div class="d-flex align-items-center">
+          <span><?= mb_strtoupper($vote['sortCode']) ?></span>
         </div>
-    <?php endforeach; ?>
-    <div class="card card-vote see-all">
-        <div class="card-body d-flex align-items-center justify-content-center">
-            <a href="<?= base_url() ?>votes/decryptes" class="stretched-link no-decoration">VOIR TOUS</a>
-        </div>
+      </div>
+      <div class="card-header d-flex flex-row justify-content-between">
+        <span class="date"><?= months_abbrev($vote['dateScrutinFR']) ?></span>
+      </div>
+      <div class="card-body d-flex flex-column justify-content-center">
+        <span class="title">
+          <a href="<?= base_url() ?>votes/legislature-<?= $vote['legislature'] ?>/vote_<?= $vote['voteNumero'] ?>" class="stretched-link no-decoration">
+            <?= $vote['voteTitre'] ?>
+          </a>
+        </span>
+        <?php if ($vote['reading']): ?>
+          <span class="reading mt-2">
+            <?= $vote['reading'] ?>
+          </span>
+        <?php endif; ?>
+      </div>
+      <div class="card-footer">
+        <span class="field badge badge-primary py-1 px-2"><?= $vote['category_libelle'] ?></span>
+      </div>
     </div>
+  <?php endforeach; ?>
+  <div class="card card-vote see-all">
+    <div class="card-body d-flex align-items-center justify-content-center">
+      <a href="<?= base_url() ?>votes/decryptes" class="stretched-link no-decoration">VOIR TOUS</a>
+    </div>
+  </div>
 </div>
