3d3bdd25e9c3be244bccba56d778f03aeb0aa3ca
awenig <awenigmar@gmail.com>
2022-02-02 17:58:08 +0100

    Dashboard

--- application/models/Parrainages_model.php
+++ application/models/Parrainages_model.php
@@ -9,6 +9,8 @@
       if ($onlyMps) {
         $this->db->where_in('mandat', array('député', 'députée'));
       }
+      $this->db->join('deputes_last', 'deputes_last.mpId = parrainages.mpId');
+      $this->db->order_by('parrainages.nameLast ASC, parrainages.nameFirst ASC');
       $query = $this->db->get('parrainages');
       return $query->result_array();
     }
