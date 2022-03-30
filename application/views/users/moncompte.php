d90fa48cb655527e4f3f780602e51e62d7f7f6fa
awenig <awenigmar@gmail.com>
2022-03-30 14:01:42 +0200

    Change page mon-compte

--- application/views/users/moncompte.php
+++ application/views/users/moncompte.php
@@ -10,8 +10,10 @@
         <div class="card-body">
           <h5 class="card-title font-weight-bold">Mes données personnelles</h5>
           <p class="mb-0">Username : <?= $user['username'] ?></p>
+          <p class="mb-0">Code psotal : <?= $user['zipcode'] ?></p>
+          <p>Type de compte : <?= $user['type'] ?></p>
           <div class="mt-3">
-            <a href="#" class="card-link">Modifier</a>
+            <a href="<?= base_url() ?>mon-compte/modifier-donnees-personnelles" class="card-link">Modifier</a>
           </div>
         </div>
       </div>
@@ -20,13 +22,16 @@
       <div class="card">
         <div class="card-body">
           <h5 class="card-title font-weight-bold">Mes données de connexion</h5>
-          <p class="mb-0">Username : <?= $user['username'] ?></p>
+          <p class="mb-0">Email : <?= $user['email'] ?></p>
           <p class="mb-0">●●●●●●●●●</p>
           <div class="mt-3">
-            <a href="#" class="card-link">Modifier</a>
+            <a href="<?= base_url() ?>mon-compte/modifier-donnees-connexion" class="card-link">Modifier</a>
           </div>
         </div>
       </div>
+    </div>
+    <div class="col-12 mt-4">
+      <a class="btn btn-danger" href="<?= base_url() ?>mon-compte/supprimer-compte">Supprimer mon compte</a>
     </div>
   </div>
 </div>
