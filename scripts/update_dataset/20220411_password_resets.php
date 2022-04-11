720d4de22eb92fe9bdafd30148c83a8daceccdec
awenig <awenigmar@gmail.com>
2022-04-11 23:02:20 +0200

    Update 20220411_password_resets.php

--- scripts/update_dataset/20220411_password_resets.php
+++ scripts/update_dataset/20220411_password_resets.php
@@ -4,7 +4,7 @@
   include('../bdd-connexion.php');
 
   $bdd->query('CREATE TABLE IF NOT EXISTS `password_resets` (
-    `id` int(11) NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
     `token` VARCHAR(255) UNIQUE NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
