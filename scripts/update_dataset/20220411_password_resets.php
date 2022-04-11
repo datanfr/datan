cd71e1bb6b00b750fbc05f398e45943fc4b5823e
awenig <awenigmar@gmail.com>
2022-04-11 23:00:00 +0200

    Create new table password_resets

---
+++ scripts/update_dataset/20220411_password_resets.php
@@ -0,0 +1,11 @@
+<?php
+  // Add a 'reading' field to the votes_datan database
+
+  include('../bdd-connexion.php');
+
+  $bdd->query('CREATE TABLE IF NOT EXISTS `password_resets` (
+    `id` int(11) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `token` VARCHAR(255) UNIQUE NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  ) ENGINE=MyISAM CHARSET=utf8');
