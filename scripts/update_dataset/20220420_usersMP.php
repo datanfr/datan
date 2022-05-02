6e5a1a04c8df6eda433c12b6df2622ae959bc91b
awenig <awenigmar@gmail.com>
2022-05-02 18:06:49 +0200

    Add unique on username

--- scripts/update_dataset/20220420_usersMP.php
+++ scripts/update_dataset/20220420_usersMP.php
@@ -2,12 +2,24 @@
   include('../bdd-connexion.php');
 
   // Create table usersMP
-  $bdd->query('DROP TABLE IF EXISTS `usersMP`');
-  $bdd->query("CREATE TABLE `usersMP` (
-    `user` INT(11) NOT NULL ,
-    `mpId` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-    INDEX `user_idx` (`user`)
-  ) ENGINE = MyISAM;");
+  try {
+    $bdd->query("SELECT 1 FROM usersMP LIMIT 1");
+  } catch (Exception $e) {
+    // We got an exception (table not found)
+    //return FALSE;
+    $bdd->query("CREATE TABLE `usersMP` (
+      `user` INT(11) NOT NULL ,
+      `mpId` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+      INDEX `user_idx` (`user`)
+    ) ENGINE = MyISAM;");
+  }
 
   $bdd->query('ALTER TABLE `elect_deputes_candidats` CHANGE `district` `district` VARCHAR(5) NULL DEFAULT NULL;');
-  $bdd->query('ALTER TABLE `elect_deputes_candidats` ADD `candidature` INT(5) NULL DEFAULT '1' AFTER `election`;');
+
+  try {
+    $bdd->query("SELECT candidature FROM elect_deputes_candidats LIMIT 1");
+  } catch (\Exception $e) {
+    $bdd->query('ALTER TABLE `elect_deputes_candidats` ADD `candidature` INT(5) NULL DEFAULT 1 AFTER `election`;');
+  }
+
+  $bdd->query('ALTER TABLE users ADD UNIQUE (username);');
