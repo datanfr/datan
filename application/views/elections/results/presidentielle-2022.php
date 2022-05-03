51581790c10ffb427e32e36c5261486e35c07718
awenig <awenigmar@gmail.com>
2022-05-03 17:15:04 +0200

    Resultats presidentielles 2022

--- application/views/elections/results/presidentielle-2022.php
+++ application/views/elections/results/presidentielle-2022.php
@@ -1,19 +1,20 @@
-<h2 class="my-4">Résultats de l'élection présientielle de 2017</h2>
-<p>L'élection présidentielle de 2017 s'est déroulée les dimanches 23 avril et 7 mai.</p>
-<p>Le président sortant, François Hollande, a décidé de ne pas se représenter. Au total, <b>onze candidats</b> ont été validées par le Conseil constitutionnel.</p>
-<p>Au premier tour, <b>Emmanuel Macron</b> (En Marche) arrive en tête avec 24,01 % des voix, suivi de <b>Marine Le Pen</b> (Front national), avec 21,30 % des voix. Ils sont suivis de François Fillon (Les Républicains) et de Jean-Luc Mélenchon (France insoumise). Les deux candidats arrivés en tête (Emmanuel Macron et Marine Le Pen) sont sélectionnés pour le second tour.</p>
-<p>Au second tour, <b>Emmanuel Macron</b> est élu président avec 66,10 % des voix.</p>
+<h2 class="my-4">Résultats de l'élection présientielle de 2022</h2>
+<p>L'élection présidentielle de 2022 s'est déroulée les dimanches 10 et 24 avril.</p>
+<p>Au total, <b>douze candidats</b> ont été validées par le Conseil constitutionnel. Au total, 530 députés <a href="<?= base_url() ?>parrainages-2022" target="_blank">ont décidé de parrainer un candidat</a>.</p>
+<p>Au premier tour, <b>Emmanuel Macron</b> (En Marche) est arrivé en tête avec 28 % des suffrages, suivi de <b><a href="<?= base_url() ?>deputes/pas-de-calais-62/depute_marine-lepen" target="_blank">Marine Le Pen</a></b> (Rassemblement national), avec 23 % des suffrages.</p>
+<p>Ils ont été suivis de <a href="<?= base_url() ?>deputes/bouches-du-rhone-13/depute_jeanluc-melenchon" target="_blank">Jean-Luc Mélenchon</a> (La France insoumise) et d'Éric Zemmour (Reconquête). Les deux candidats arrivés en tête sont sélectionnés pour le second tour.</p>
+<p>Au second tour, <b>Emmanuel Macron</b> a été élu président avec 59 % des suffrages exprimés.</p>
 <div class="resultsPresidentielle d-flex flex-column mt-4">
   <div class="d-flex align-items-center">
     <img src="<?= asset_url() ?>imgs/photos/macron.jpg" alt="Photo Emmanuel Macron">
     <div class="d-flex flex-column ml-3 w-100">
       <p>Emmanuel Macron</p>
       <div class="chart-election test-border" style="width: 100%; height: 40px;">
-        <div class="majority d-flex align-items-center" style="flex-basis: 66%">
-          <span>66%</span>
+        <div class="majority d-flex align-items-center" style="flex-basis: 59%">
+          <span>59 %</span>
         </div>
         <div class="line"></div>
-        <div class="minority" style="flex-basis: 34%"></div>
+        <div class="minority" style="flex-basis: 41%"></div>
       </div>
     </div>
   </div>
@@ -22,11 +23,11 @@
     <div class="d-flex flex-column ml-3 w-100">
       <p>Marine Le Pen</p>
       <div class="chart-election" style="width: 100%; height: 40px;">
-        <div class="majority d-flex align-items-center" style="flex-basis: 34%">
-          <span>34%</span>
+        <div class="majority d-flex align-items-center" style="flex-basis: 41%">
+          <span>41%</span>
         </div>
         <div class="line"></div>
-        <div class="minority" style="flex-basis: 66%"></div>
+        <div class="minority" style="flex-basis: 59%"></div>
       </div>
     </div>
   </div>
