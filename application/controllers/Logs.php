52ca4ca22ee2575a15a9c987da566ff94ab6cddd
awenig <awenigmar@gmail.com>
2022-06-16 17:27:46 +0200

    Update Logs.php

--- application/controllers/Logs.php
+++ application/controllers/Logs.php
@@ -8,6 +8,7 @@
     public function index($name){
       //$file = file_get_contents( "C:/wamp64/www/files_datan/logs/daily.log" );
       $file = dirname($_SERVER['DOCUMENT_ROOT']) . '/logs/' . $name . '.log';
+      echo $file;
       //echo '<pre>' . $file . '</pre>';
 
     }
