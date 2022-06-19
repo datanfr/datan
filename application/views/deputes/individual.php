423c74954962b295d98dd9f14accd7e4b6abe51f
awenig <awenigmar@gmail.com>
2022-06-19 13:57:49 +0200

    Update MP individual page

--- application/views/deputes/individual.php
+++ application/views/deputes/individual.php
@@ -22,6 +22,9 @@
                 <h2>🗳️ Législatives 2022</h2>
                 <?php if ($electionFeature['elected'] == "1"): ?>
                   <p class="mb-0"><?= $title ?> était candidat<?= $gender['e'] ?> à sa réélection. <span class="font-weight-bold"><?= ucfirst($gender['pronom']) ?> a été réélu<?= $gender['e'] ?></span>.</p>
+                  <p class="mb-0">Sa page Datan sera prochainement mise à jour pour son nouveau mandat.</p>
+                <?php elseif ($electionFeature['elected'] == "0"): ?>
+                  <p class="mb-0"><?= $title ?> était candidat<?= $gender['e'] ?> à sa réélection. <span class="font-weight-bold"><?= ucfirst($gender['pronom']) ?> a été éliminé<?= $gender['e'] ?></span>.</p>
                 <?php elseif ($electionFeature['secondRound'] == "1"): ?>
                   <p class="mb-0"><?= $title ?> est candidat<?= $gender['e'] ?> à sa réélection. <span class="font-weight-bold"><?= ucfirst($gender['pronom']) ?> a été qualifié<?= $gender['e'] ?></span> pour le second tour.</p>
                 <?php elseif ($electionFeature['secondRound'] == "0"): ?>
