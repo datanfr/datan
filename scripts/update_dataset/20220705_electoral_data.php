133ab5eaa23cd6ec4805c7046c1f1dc74119e8ef
awenig <awenigmar@gmail.com>
2022-07-05 12:17:58 +0200

    Add new table elect_legislatives_infos

---
+++ scripts/update_dataset/20220705_electoral_data.php
@@ -0,0 +1,8 @@
+<?php
+  include('../bdd-connexion.php');
+
+  $bdd->query('DROP TABLE IF EXISTS elect_2017_leg_results');
+  $bdd->query('DROP TABLE IF EXISTS elect_2017_leg_infos');
+
+  $file = file_get_contents('sql/elect_legislatives_infos.sql');
+  $bdd->exec($file);
