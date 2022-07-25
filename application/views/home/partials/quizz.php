a1d599871441a910a9ee603532fd4a2074f4d04b
awenig <awenigmar@gmail.com>
2022-07-25 11:26:47 +0200

    Remove quiz on homepage

---
+++ application/views/home/partials/quizz.php
@@ -0,0 +1,20 @@
+<div class="row bloc-quiz py-4">
+  <div class="container p-md-0">
+    <div class="row py-4">
+      <div class="col-12">
+        <h2 class="text-center pb-4">Mon député et moi</h2>
+        <h3 class="text-center pb-4">Le <span class="text-primary">quiz</span> spécial élections législatives 2022</h3>
+        <div class="row pt-4">
+          <div class="d-flex align-items-start justify-content-center col-lg-5 col-md-6 col-12 offset-lg-1">
+            <object style="height: 20rem" data="<?= asset_url()."imgs/quiz/phone.svg" ?>" type="image/svg+xml"></object>
+          </div>
+          <div class="d-flex flex-column align-items-center justify-content-around col-md-6 col-lg-5 col-12 mt-5 mt-md-0">
+            <h3 class="mb-3 text-center my-3">Ai-je les mêmes positions que mon député ?</h3>
+            <p class="text-center my-3">Prenez position sur 20 votes de l'Assemblée nationale et découvrez votre proximité avec votre député.</p>
+            <span class="url_obf btn btn-primary my-3" target="_blank" url_obf="<?= url_obfuscation("https://quiz.datan.fr") ?>">Je fais le quiz !</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
