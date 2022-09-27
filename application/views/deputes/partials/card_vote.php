11f78c11647527419dbf4b88b6361567fc5974a2
awenig <awenigmar@gmail.com>
2022-09-27 10:04:47 +0200

    Tests for design MP vote card

--- application/views/deputes/partials/card_vote.php
+++ application/views/deputes/partials/card_vote.php
@@ -1,4 +1,4 @@
-<div class="card card-vote">
+<div class="card card-vote" style="width: 310px">
   <div class="thumb d-flex align-items-center <?= $vote['vote_depute'] ?>">
     <div class="d-flex align-items-center">
       <span><?= mb_strtoupper($vote['vote_depute']) ?></span>
@@ -18,14 +18,23 @@
     <?php endif; ?>
   </div>
   <?php if ($vote['explication']): ?>
-    <div class="card-footer d-flex justify-content-end">
+    <div class="card-footer d-flex justify-content-between">
       <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#explication-l<?= $vote['legislature'] ?>-v<?= $vote['voteNumero'] ?>">
-        L'explication
+        L'avis du député
       </button>
+      <a type="button" class="btn btn-primary" href="<?= base_url() ?>votes/legislature-<?= $vote['legislature'] ?>/vote_<?= $vote['voteNumero'] ?>">+ d'infos</a>
     </div>
   <?php else: ?>
     <div class="card-footer">
       <span class="field badge badge-primary py-1 px-2"><?= $vote['category_libelle'] ?></span>
+    </div>
+  <?php endif; ?>
+  <?php if (!$vote['explication']): ?>
+    <div class="bg-primary py-2 d-flex justify-content-center align-items-center" style="border-bottom-left-radius: 5px; border-bottom-right-radius: 5px">
+      <div class="" style="color: #fff">
+        <?= file_get_contents(asset_url()."imgs/icons/box-arrow-up-right.svg") ?>
+      </div>
+      <a href="#" class="no-decoration text-center font-weight-bold ml-2" style="color: #fff">L'avis du député</a>
     </div>
   <?php endif; ?>
 </div>
