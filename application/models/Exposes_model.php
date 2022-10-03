3faed34b092f93fe4e83eb8d73fa52115b6a7b33
awenig <awenigmar@gmail.com>
2022-10-03 09:34:21 +0200

    System for changing expose (admin dashboard)

---
+++ application/models/Exposes_model.php
@@ -0,0 +1,27 @@
+<?php
+  class Exposes_model extends CI_Model {
+    public function __construct() {
+      $this->load->database();
+    }
+
+    public function get_all_exposes(){
+      $this->db->order_by('exposeSummaryPublished');
+      return $this->db->get('exposes')->result_array();
+    }
+
+    public function get_expose($id){
+      return $this->db->get_where('exposes', array('id' => $id))->row_array();
+    }
+
+    public function modify($legislature, $voteNumero){
+      $data = array(
+        'exposeSummaryPublished' => $this->input->post('exposeSummary'),
+      );
+
+      $this->db->set($data);
+      $this->db->where('legislature', $legislature);
+      $this->db->where('voteNumero', $voteNumero);
+      $this->db->update('exposes');
+    }
+
+  }
