ef7297aa448c46fb95901555804b21493fc66d3c
Remi <rmikel@clever-age.com>
2022-10-21 19:36:17 +0200

    Still fixes needed

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -5,10 +5,10 @@ sed -i 's/${MYSQL_USER}/'"$MYSQL_USER"'/g' /etc/apache2/sites-available/000-defa
 sed -i 's/${MYSQL_PASSWORD}/'"$MYSQL_PASSWORD"'/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/${MYSQL_DATABASE}/'"$MYSQL_DATABASE"'/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/${DATABASE_HOST}/'"$DATABASE_HOST"'/g' /etc/apache2/sites-available/000-default.conf
-sed -i 's/${BASE_URL}/'"$BASE_URL"'/g' /etc/apache2/sites-available/000-default.conf
+# sed -i 's/${BASE_URL}/'"$BASE_URL"'/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/${API_KEY_NOBG}/'"$API_KEY_NOBG"'/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/${API_KEY_MAILJET}/'"$API_KEY_MAILJET"'/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/${API_KEY_SECRETE_MAILJET}/'"$API_KEY_SECRETE_MAILJET"'/g' /etc/apache2/sites-available/000-default.conf
-sed -i 's/${COMPOSER_AUTOLOAD}/'"$COMPOSER_AUTOLOAD"'/g' /etc/apache2/sites-available/000-default.conf
+# sed -i 's/${COMPOSER_AUTOLOAD}/'"$COMPOSER_AUTOLOAD"'/g' /etc/apache2/sites-available/000-default.conf
 
 exec "$@"
