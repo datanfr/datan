4a6b774bcd5b710a05c74bdf46bff15b0839947b
awenig <awenigmar@gmail.com>
2022-11-04 09:34:57 +0100

    Fix bug banner for filter votes on mobiles

--- application/views/groupes/votes_datan.php
+++ application/views/groupes/votes_datan.php
@@ -14,7 +14,7 @@
   <?php if (!empty($groupe['couleurAssociee'])): ?>
     <div class="liseret-groupe" style="background-color: <?= $groupe['couleurAssociee'] ?>"></div>
   <?php endif; ?>
-  <div class="d-none d-lg-none justify-content-between align-items-center sticky-top p-3" data-toggle="modal" data-target="#filterModal" id="filterBanner" style="top: 100px">
+  <div class="d-none d-lg-none justify-content-between align-items-center sticky-top p-3" data-toggle="modal" data-target="#filterModal" id="filterBanner" style="top: 50px">
     <span class="text-white font-weight-bold">Filtrer par catégorie</span>
     <?= file_get_contents(asset_url().'imgs/icons/funnel-fill.svg') ?>
   </div>
