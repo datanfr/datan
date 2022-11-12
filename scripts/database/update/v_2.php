64c77c57192570f8006a10bd9c3188dcbbda122c
awenig <awenigmar@gmail.com>
2022-11-12 09:19:37 +0100

    Create new table: update db

---
+++ scripts/database/update/v_2.php
@@ -0,0 +1,54 @@
+<?php
+
+  /*
+    With this file, the new version of the database will be => XX
+    The name of the file needs to be v_X.php
+
+    What needs to be changed in this file:
+    1. CHANGE THE $new_v value
+    2. MAKE YOUR CHANGE TO THE DATABASE
+
+  */
+
+  include '../../bdd-connexion.php';
+
+  /* 1. CHANGE THE new_v VALUE */
+  $new_v = 2;
+
+  $last_v_installed = $bdd->query('SELECT version FROM mysql_v ORDER BY version DESC LIMIT 1');
+  $last_v_installed = $last_v_installed->fetchAll(PDO::FETCH_ASSOC);
+  $last_v_installed = $last_v_installed[0]['version'];
+
+  echo "New version to install = " . $new_v . "<br>";
+  echo "Last version installed = " . $last_v_installed . "<br>";
+
+  if ($new_v > $last_v_installed) {
+    echo "Install the new table: class_groups_month <br>";
+
+    /* 2. MAKE YOUR CHANGE TO THE DATABASE */
+    $prepare = $bdd->prepare("CREATE TABLE `datan`.`class_groups_month`(
+      `organeRef` VARCHAR(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+      `legislature` INT(2) NOT NULL ,
+      `active` INT(1) NOT NULL ,
+      `dateValue` DATE NOT NULL ,
+      `value` DECIMAL(6,3) NOT NULL ,
+      `votes` BIGINT(21) NOT NULL ,
+      `dateMaj` DATE NOT NULL
+    )");
+
+    if (!$prepare->execute()) {
+      echo "The update did not work <br>";
+    } else {
+      echo "The update worked";
+
+      $prepare = $bdd->prepare("INSERT INTO mysql_v (version) VALUES (:new_version)");
+      $prepare->execute(array('new_version' => $new_v));
+
+    }
+
+  } else {
+    echo "The last version installed is already up to date <br>";
+    die();
+  }
+
+?>
