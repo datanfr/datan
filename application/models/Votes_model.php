f91a551f510ca41d2097c166164922222fb909df
awenig <awenigmar@gmail.com>
2022-11-22 11:07:03 +0100

    Change modified_at when MP modify an explanation

--- application/models/Votes_model.php
+++ application/models/Votes_model.php
@@ -659,7 +659,7 @@
       $this->db->select('d.civ, d.nameFirst, d.nameLast, d.nameUrl, d.dptSlug, substr(d.mpId, 3) AS idImage, d.img, e.text, v.vote');
       $this->db->join('deputes_last d', 'e.mpId = d.mpId', 'left');
       $this->db->join('votes_scores v', 'v.legislature = e.legislature AND v.voteNumero = e.voteNumero AND v.mpId = e.mpId', );
-      $this->db->order_by('e.id', 'des');
+      $this->db->order_by('e.modified_at', 'DESC');
       $this->db->where('state', 1);
       $results = $this->db->get_where('explications_mp e', $where)->result_array();
       foreach ($results as $key => $value) {
