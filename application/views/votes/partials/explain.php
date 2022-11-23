9095a7b594f1036e2f4f279f31a4004a171c7150
awenig <awenigmar@gmail.com>
2022-11-23 09:36:43 +0100

    Add link to photo, change design, add MP's group

--- application/views/votes/partials/explain.php
+++ application/views/votes/partials/explain.php
@@ -10,30 +10,30 @@ $widthCol = isset($explain[0]) ? 4 : 6;
             </div>
             <div class="col-lg-<?= $widthCol ?>">
                 <p class="text-center font-weight-bold sort-adopté h5">LES DÉPUTÉS POUR</p>
-                <div class="card mt-4">
-                    <div class="card-body read-more-container py-2">
+                <div class="card mt-4 py-0">
+                    <div class="card-body read-more-container py-0 mt-3">
                         <?php if (isset($explain[1])) : ?>
                             <?php foreach ($explain[1] as $exp) :
                                 $this->load->view('votes/partials/explain_text.php', array('exp' => $exp));
                             endforeach ?>
                             <p class="read-more-button"><a href="#" class="btn btn-primary">Lire plus</a></p>
                         <?php else : ?>
-                            <i>Aucun député <b>pour</b> ne sait encore exprimé.</i>
+                            <p><i>Aucun député <b>pour</b> ne sait encore exprimé.</i></p>
                         <?php endif ?>
                     </div>
                 </div>
             </div>
             <div class="col-lg-<?= $widthCol ?> mt-5 mt-lg-0">
                 <p class="text-center font-weight-bold sort-rejeté h5">LES DÉPUTÉS CONTRE</p>
-                <div class="card mt-4">
-                    <div class="card-body read-more-container py-0">
+                <div class="card mt-4 py-0">
+                    <div class="card-body read-more-container py-0 mt-3">
                         <?php if (isset($explain[-1])) : ?>
                             <?php foreach ($explain[-1] as $exp) :
                                 $this->load->view('votes/partials/explain_text.php', array('exp' => $exp));
                             endforeach ?>
                             <p class="read-more-button"><a href="#" class="btn btn-primary">Lire plus</a></p>
                         <?php else : ?>
-                            <i>Aucun député <b>contre</b> ne sait encore exprimé.</i>
+                            <p><i>Aucun député <b>contre</b> ne sait encore exprimé.</i></p>
                         <?php endif ?>
                     </div>
                 </div>
@@ -41,8 +41,8 @@ $widthCol = isset($explain[0]) ? 4 : 6;
             <?php if (isset($explain[0])) : ?>
                 <div class="col-lg-<?= $widthCol ?> mt-5 mt-lg-0">
                     <p class="text-center font-weight-bold sort-abstention h5">LES DÉPUTÉS ABSTENUS</p>
-                    <div class="card mt-4">
-                        <div class="card-body read-more-container py-0">
+                    <div class="card mt-4 py-0">
+                        <div class="card-body read-more-container py-0 mt-3">
                             <?php foreach ($explain[0] as $exp) :
                                 $this->load->view('votes/partials/explain_text.php', array('exp' => $exp));
                             endforeach ?>
