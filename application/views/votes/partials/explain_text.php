c44d2cab54b547e122c89a0885044a719faa960a
awenig <awenigmar@gmail.com>
2022-11-23 09:36:43 +0100

    Add link to photo, change design, add MP's group

--- application/views/votes/partials/explain_text.php
+++ application/views/votes/partials/explain_text.php
@@ -1,11 +1,13 @@
-<div class="d-flex align-items-start py-3">
+<div class="d-flex align-items-start my-3">
     <div class="depute-img-circle">
         <?php if ($exp['img']) : ?>
-            <picture>
-                <source srcset="<?= asset_url(); ?>imgs/deputes_nobg_webp/depute_<?= $exp['idImage'] ?>_webp.webp" type="image/webp">
-                <source srcset="<?= asset_url(); ?>imgs/deputes_nobg/depute_<?= $exp['idImage'] ?>.png" type="image/png">
-                <img src="<?= asset_url(); ?>imgs/deputes_original/depute_<?= $exp['idImage'] ?>.png" width="150" height="192" alt="<?= $title ?>">
-            </picture>
+            <a href="<?= base_url() . "deputes/" . $exp['dptSlug'] . '/depute_' . $exp['nameUrl'] ?>">
+              <picture>
+                  <source srcset="<?= asset_url(); ?>imgs/deputes_nobg_webp/depute_<?= $exp['idImage'] ?>_webp.webp" type="image/webp">
+                  <source srcset="<?= asset_url(); ?>imgs/deputes_nobg/depute_<?= $exp['idImage'] ?>.png" type="image/png">
+                  <img src="<?= asset_url(); ?>imgs/deputes_original/depute_<?= $exp['idImage'] ?>.png" width="150" height="192" alt="<?= $title ?>">
+              </picture>
+            </a>
         <?php else : ?>
             <picture>
                 <source srcset="<?= asset_url() ?>imgs/placeholder/placeholder-face-2.png" type="image/png">
@@ -16,9 +18,10 @@
     <div class="ml-3">
         <p class="mb-0">
             <a class="font-weight-bold no-decoration underline" href="<?= base_url() . "deputes/" . $exp['dptSlug'] . '/depute_' . $exp['nameUrl'] ?>" class="pg-depute-all">
-                <?= $exp['nameFirst'] . ' ' . $exp['nameLast'] ?> -
+                <?= $exp['nameFirst'] . ' ' . $exp['nameLast'] ?>
             </a>
-            <?= $exp['text'] ?>
+            - <span style="color: <?= $exp['couleurAssociee'] ?>"><?= $exp['libelleAbrev'] ?></span>
+            - <?= $exp['text'] ?>
         </p>
     </div>
 </div>
