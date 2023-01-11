b3b93da1e371f28039571351df9bb935fdaf48e6
awenig <awenigmar@gmail.com>
2023-01-11 15:40:57 +0100

    Fix bug chart election on MPs' pages

--- application/views/deputes/individual.php
+++ application/views/deputes/individual.php
@@ -321,42 +321,40 @@
                 </div>
                 <h3 class="mt-4">L'élection de <?= $title ?> en détail</h3>
                 <span class="subtitle"><?= $election_result['tour_election'] ?> tour des élections législatives de 2022 - <?= $depute["circo"] ?><sup><?= $depute["circo_abbrev"] ?></sup> circonscription <?= $depute['dptLibelle2'] ?><?= $depute['departementNom'] ?></span>
-                <div class="row row-chart-election mt-4">
-                  <div class="col-md-5 d-flex flex-column justify-content-center">
+                <div class="d-flex flex-column mt-4">
+                  <div class="d-flex flex-column justify-content-center">
                     <p>Il y avait dans  la circonscription <b><?= formatNumber($election_infos['inscrits']) ?> personnes inscrites</b> sur les listes électorales.</p>
                     <p>Pendant le <?= $election_result['tour_election'] ?> tour, le taux d'abstention était de <?= $election_infos['abstention_rate'] ?> %. Au niveau national, il était de 57 %.</p>
                     <p><?= $title ?> a été élu<?= $gender['e'] ?> avec <?= formatNumber($election_result['voix']) ?> voix, soit <?= round($election_result['voix'] * 100 / $election_infos['inscrits']) ?>% des inscrits.</p>
                     <p>Plus d'information ? <span class="url_obf" url_obf="<?= url_obfuscation("https://www.interieur.gouv.fr/Elections/Les-resultats/Legislatives/elecresult__legislatives-2022/(path)/legislatives-2022/" . $election_infos['dpt_url'] . "/" . $election_infos['dpt_url'] . "" . $election_infos['circo_url'] .".html") ?>">Cliquez ici.</span></p>
                   </div>
-                  <div class="col-md-7 p-0 d-flex">
-                    <div class="bar-container election py-4 px-2 pr-md-1 pl-md-0" id="pattern_background">
-                      <p class="text-center title">Le choix électoral des <u><?= formatNumber($election_infos['inscrits']) ?> inscrits</u></p>
-                      <div class="chart">
-                        <div class="bar-chart d-flex justify-content-between align-items-end">
-                          <div class="bars mx-1 mx-md-3" style="height: <?= round($election_result['voix'] / $election_infos['inscrits'] * 100) ?>%">
-                            <span class="score text-center"><?= formatNumber($election_result['voix']) ?></span>
+                  <div class="bar-container stats election mt-3 p-3" id="pattern_background">
+                    <p class="text-center title mb-0">Le choix électoral des <u><?= formatNumber($election_infos['inscrits']) ?> inscrits</u></p>
+                    <div class="chart">
+                      <div class="bar-chart d-flex justify-content-between align-items-end">
+                        <div class="bars mx-1 mx-md-3" style="height: <?= round($election_result['voix'] / $election_infos['inscrits'] * 100) ?>%">
+                          <span class="score text-center"><?= formatNumber($election_result['voix']) ?></span>
+                        </div>
+                        <?php if (isset($election_opponent)): ?>
+                          <div class="bars mx-1 mx-md-3" style="height: <?= round($election_opponent['voix'] / $election_infos['inscrits'] * 100) ?>%">
+                            <span class="score text-center"><?= formatNumber($election_opponent['voix']) ?></span>
                           </div>
-                          <?php if (isset($election_opponent)): ?>
-                            <div class="bars mx-1 mx-md-3" style="height: <?= round($election_opponent['voix'] / $election_infos['inscrits'] * 100) ?>%">
-                              <span class="score text-center"><?= formatNumber($election_opponent['voix']) ?></span>
-                            </div>
-                          <?php endif; ?>
-                          <div class="bars mx-1 mx-md-3" style="height: <?= round(($election_infos['blancs'] + $election_infos['nuls']) / $election_infos['inscrits'] * 100) ?>%">
-                            <span class="score text-center"><?= formatNumber($election_infos['blancs'] + $election_infos['nuls']) ?></span>
-                          </div>
-                          <div class="bars mx-1 mx-md-3" style="height: <?= round($election_infos['abstentions'] / $election_infos['inscrits'] * 100) ?>%">
-                            <span class="score text-center"><?= formatNumber($election_infos['abstentions']) ?></span>
-                          </div>
+                        <?php endif; ?>
+                        <div class="bars mx-1 mx-md-3" style="height: <?= round(($election_infos['blancs'] + $election_infos['nuls']) / $election_infos['inscrits'] * 100) ?>%">
+                          <span class="score text-center"><?= formatNumber($election_infos['blancs'] + $election_infos['nuls']) ?></span>
+                        </div>
+                        <div class="bars mx-1 mx-md-3" style="height: <?= round($election_infos['abstentions'] / $election_infos['inscrits'] * 100) ?>%">
+                          <span class="score text-center"><?= formatNumber($election_infos['abstentions']) ?></span>
                         </div>
                       </div>
-                      <div class="d-flex justify-content-between mt-2">
-                        <div class="legend-element text-center mx-1"><?= $title ?></div>
-                        <?php if (isset($election_opponent)): ?>
-                          <div class="legend-element text-center mx-1"><?= $election_opponent['candidat'] ?></div>
-                        <?php endif; ?>
-                        <div class="legend-element text-center mx-1">Blancs et nuls</div>
-                        <div class="legend-element text-center mx-1">Abstentions</div>
-                      </div>
+                    </div>
+                    <div class="d-flex justify-content-between mt-2">
+                      <div class="legend-element text-center mx-1"><?= $title ?></div>
+                      <?php if (isset($election_opponent)): ?>
+                        <div class="legend-element text-center mx-1"><?= $election_opponent['candidat'] ?></div>
+                      <?php endif; ?>
+                      <div class="legend-element text-center mx-1">Blancs et nuls</div>
+                      <div class="legend-element text-center mx-1">Abstentions</div>
                     </div>
                   </div>
                 </div>
