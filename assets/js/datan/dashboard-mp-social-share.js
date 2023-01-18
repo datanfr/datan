6185cd78b029d74a01aff6625a12797b33bac362
awenig <awenigmar@gmail.com>
2023-01-18 21:36:06 +0100

    Continue working on social-share btns

--- assets/js/datan/dashboard-mp-social-share.js
+++ assets/js/datan/dashboard-mp-social-share.js
@@ -4,27 +4,18 @@ function socialWindow(url) {
   var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
   window.open(url,"NewWindow",params);
 }
-var pageUrl = encodeURIComponent(document.URL);
-//var tweet = encodeURIComponent(jQuery("meta[property='og:description']").attr("content"));
-var tweet = "A l'Assemblée, j'ai voté [pour/contre] le scrutin : « [title_scrutin] ». Découvrez mon explication de vote sur ma page Datan ! #DirectAN [URL]";
-var whatsappMessage = "Je viens de découvrir un nouveau vote de l'Assemblée nationale sur Datan ! Découvre le aussi : " + document.URL;
 
-jQuery(".social-share.facebook").on("click", function() {
-    var url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
-    socialWindow(url);
-});
+var pageUrl = encodeURIComponent(document.URL);
+
 
 jQuery(".social-share.twitter").on("click", function() {
-    var url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + tweet;
-    socialWindow(url);
+  // Get values from the vote
+  let legislature = $(this).attr("data-legislature");
+  let voteNumero = $(this).attr("data-voteNumero");
+  let position = $(this).attr("data-position");
+  let title = $(this).attr("data-title");
+  let tweet = "À l'Assemblée, " + position + " lors du scrutin « " + title + " ». Découvrez mon explication de vote sur Datan ! [hashtagDirectAN]";
+
+  let url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + tweet;
+  socialWindow(url);
 });
-
-jQuery(".social-share.linkedin").on("click", function() {
-    var url = "https://www.linkedin.com/sharing/share-offsite/?url=" + pageUrl;
-    socialWindow(url);
-})
-
-jQuery(".social-share.whatsapp").on("click", function() {
-    var url = "whatsapp://send?text=" + whatsappMessage;
-    socialWindow(url);
-})
