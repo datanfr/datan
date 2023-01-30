cf893ca903272a99ac11e19dd34238950111ced3
awenig <awenigmar@gmail.com>
2023-01-30 09:47:39 +0100

    pg depute

--- application/views/partials/share.php
+++ application/views/partials/share.php
@@ -1,18 +1,18 @@
 <div class="d-flex flex-row flex-wrap social-share-bloc">
   <button type="button" name="button" class="btn social-share twitter twitter-bg d-flex">
-    <img src="<?= asset_url() ?>imgs/logos/twitter-no-round.png" alt="Partagez sur Twitter">
+    <img src="<?= asset_url() ?>imgs/logos/twitter-no-round.png" alt="Partagez sur Twitter" width="24" height="20">
     <span>Twitter</span>
   </button>
   <button type="button" name="button" class="btn social-share facebook fcb-bg d-flex">
-    <img src="<?= asset_url() ?>imgs/logos/facebook-no-round.png" alt="Partagez sur Twitter">
+    <img src="<?= asset_url() ?>imgs/logos/facebook-no-round.png" alt="Partagez sur Twitter" width="20" height="10">
     <span>Facebook</span>
   </button>
   <button type="button" name="button" class="btn social-share linkedin linkedin-bg d-flex">
-    <img src="<?= asset_url() ?>imgs/logos/linkedin-no-round.png" alt="Partagez sur Linkedin">
+    <img src="<?= asset_url() ?>imgs/logos/linkedin-no-round.png" alt="Partagez sur Linkedin" width="20" height="20">
     <span>Linkedin</span>
   </button>
   <button type="button" name="button" class="mobileShow btn social-share whatsapp whatsapp-bg">
-    <img src="<?= asset_url() ?>imgs/logos/whatsapp-no-round.png" alt="Partagez sur Whatsapp">
+    <img src="<?= asset_url() ?>imgs/logos/whatsapp-no-round.png" alt="Partagez sur Whatsapp" width="20" height="20">
     <span>Whatsapp</span>
   </button>
 </div>
