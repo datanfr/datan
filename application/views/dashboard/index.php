c0b2adbed975d15f470208d7d3adf247e73c786c
awenig <awenigmar@gmail.com>
2023-07-08 14:48:35 +0200

    Remove breadcrumb admin dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -5,12 +5,6 @@
           <div class="col-sm-6">
             <h1 class="m-0 text-dark">Dashboard</h1>
           </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div>
         </div>
       </div>
     </div>
