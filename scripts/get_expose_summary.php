dee848fc3a1d5f5ffd634dd92eae50f94adc2038
awenig <awenigmar@gmail.com>
2023-09-28 17:46:13 +0200

    start working w/ chat-gpt

--- scripts/get_expose_summary.php
+++ scripts/get_expose_summary.php
@@ -32,8 +32,17 @@
 
     echo $voteNumero;
 
-    $html = file_get_html("https://datan.fr/votes/legislature-" . $legislature . "/vote_" . $voteNumero);
-    $exposeMotifs = $html->find('#exposeMotifs', 0);
+
+    //$html = file_get_html("https://datan.fr/votes/legislature-" . $legislature . "/vote_" . $voteNumero);
+    //$exposeMotifs = $html->find('#exposeMotifs', 0);
+
+    $exposeMotifs = "Les auteurs de cet amendement souhaitent supprimer la mise en œuvre d'une redevance de 30 euros pour les candidats, notamment ultramarins, de l’examen annuel de capacité professionnelle pour l’accès à la profession de transporteur routier de marchandises, de personnes et de commissionnaires. Instituer une nouvelle redevance dans un contexte d'inflation n'est pas envisageable, en particulier face à la cherté de la vie en outre-mer.
+
+La ratification d'un décret ne laissant pas de marge de manœuvre aux parlementaires pour faire varier les conditions dudit décret, il est proposé de supprimer intégralement l'article autorisant la ratification.
+
+L'objet est avant tout de protéger les potentiels candidats de cet examen de cette redevance supplémentaire.";
+
+echo $exposeMotifs;
 
     if ($exposeMotifs) {
       $exposeMotifs = strip_tags($exposeMotifs);
@@ -42,20 +51,65 @@
 
       // CURL
 
-      $ch = curl_init();
+      $request_body = [
+        "messages" => [
+          [
+            "role" => "assistant",
+            "content" => "Qui est le président de la France ?"
+            ]
+          ],
+        "model" => "gpt-3.5-turbo",
+        "max_tokens" => 100,
+        "temperature" => 0.7,
+        "top_p" => 1,
+        "presence_penalty" => 0.75,
+        "frequency_penalty"=> 0.75,
+        "stream" => false,
+      ];
 
-      curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"model\": \"text-davinci-002\", \"prompt\": \"Réécrire ce texte en 100 mots : " . $exposeMotifs . "\", \"temperature\": 0.7, \"max_tokens\": 256, \"top_p\": 1, \"frequency_penalty\": 0.5, \"presence_penalty\": 0.55}");
+      var_dump($request_body);
 
-      $headers = array();
-      $headers[] = 'Content-Type: application/json';
-      $headers[] = 'Authorization: Bearer ' . $_SERVER['OPEN_AI_KEY'];
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      $postfields = json_encode($request_body);
+      $curl = curl_init();
 
-      $result = curl_exec($ch);
+      curl_setopt_array($curl, [
+        CURLOPT_URL => "https://api.openai.com/v1/chat/completions",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $postfields,
+        CURLOPT_HTTPHEADER => [
+          'Content-Type: application/json',
+          'Authorization: Bearer ' . $_SERVER['OPEN_AI_KEY']
+        ],
+      ]);
+
+
+      //curl_setopt($ch, CURLOPT_POST, 1);
+      //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      //curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"model\": \"gpt-3.5-turbo\", \"prompt\": \"Peux-tu résumer ce texte politique en 100 mots : " . $exposeMotifs . "\", \"temperature\": 0.7, \"max_tokens\": 256, \"top_p\": 1, \"frequency_penalty\": 0.5, \"presence_penalty\": 0.55}");
+      //curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+      curl_close($curl);
+
+      if ($err) {
+        echo "Error #: " . $err;
+      } else {
+        echo $response;
+      }
+
+      die();
+
+
+
+
 
       if (curl_errno($ch)) {
         echo 'Error:' . curl_error($ch);
@@ -64,16 +118,15 @@
         echo "Open AI worked \n";
         $response = json_decode($result, TRUE);
 
+        var_dump($response);
+
         $text = $response['choices'][0]['text'];
+
+        echo $text;
 
         // Insert into database
         $insert = $bdd->prepare('INSERT INTO exposes (legislature, voteNumero, exposeOriginal, exposeSummary) VALUES (:legislature, :voteNumero, :exposeOriginal, :expose)');
-        $insert->execute(array(
-          'legislature' => $legislature,
-          'voteNumero' => $voteNumero,
-          'exposeOriginal' => $exposeMotifs,
-          'expose' => $text,
-        ));
+        //$insert->execute(array('legislature' => $legislature, 'voteNumero' => $voteNumero, 'exposeOriginal' => $exposeMotifs, 'expose' => $text));
 
       }
 
