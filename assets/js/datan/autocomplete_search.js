8b3b6b16e1b24a46f6aba1f74ae696ebbeaf55fc
awenig <awenigmar@gmail.com>
2024-03-21 20:14:53 +0100

    New dynamic placeholder

--- assets/js/datan/autocomplete_search.js
+++ assets/js/datan/autocomplete_search.js
@@ -23,3 +23,50 @@ document.getElementById("search").addEventListener("input", function() {
     document.getElementById("search-bloc").style.borderRadius = "8px";
   }
 });
+
+// Placeholder
+let input = document.querySelector("input");
+let slider = document.querySelector(".slider");
+let slider_box = document.querySelector(".slider_box");
+let placeholder = document.querySelector(".placeholder");
+
+let list = ["un député", "une ville", "un vote"];
+let word = 0;
+let intervals = "";
+text_animation();
+setintervals();
+slider_box.style.height = slider.clientHeight + "px";
+
+input.onfocus = function(){
+  placeholder.style.display = "none";
+  clearInterval(intervals);
+}
+input.onblur = function(){
+  if (input.value == "") {
+    placeholder.style.display = "flex";
+    word = 0;
+    text_animation();
+    setintervals();
+  }
+}
+
+function setintervals(){
+  intervals = setInterval(() => {
+    text_animation();
+  }, 2500);
+}
+
+function text_animation(){
+  word++;
+  slider.innerHTML = list[word - 1];
+  slider.style.opacity = "1";
+  slider.style.left = "0px";
+
+  setTimeout(() => {
+    slider.style.opacity = "0";
+    slider.style.left = "-5px";
+  }, 2000);
+  if(list.length == word) {
+    word = 0;
+  }
+}
