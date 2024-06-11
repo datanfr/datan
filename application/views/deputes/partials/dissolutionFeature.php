35f69edb4c13a9379bfa64619b68b9ce4439f468
unknown <awenigmar@gmail.com>
2024-06-11 16:11:38 +0200

    Fix bug or individual MP pages

---
+++ application/views/deputes/partials/dissolutionFeature.php
@@ -0,0 +1,10 @@
+<?php if ($depute['dateFin'] == '2024-06-09'): ?>
+  <div class="card card-election-feature information-success mb-4 mt-5 border-0" style="overflow: hidden">
+    <div class="card-body">
+      <h2>Dissolution de l'Assemblée nationale</h2>
+      <p>Le mandat de <?= $title ?> a pris fin en raison de la dissolution de l'Assemblée nationale.</p>
+      <p>Le président Emmanuel Macron a décidé de dissoudre l'Assemblée à la suite des élections européennes. La 16<sup>ème</sup> législature a officiellement pris fin le 9 juin 2024.</p>
+      <a class="btn btn-light" href="https://datan.fr/blog/actualite-politique/le-president-emmanuel-macron-annonce-la-dissolution-de-lassemblee-nationale" target="_blank">En savoir plus</a>
+    </div>
+  </div>
+<?php endif; ?>
