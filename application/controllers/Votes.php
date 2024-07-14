7fc17801856de1d4bc7d24186144f25c72cf8c94
Awenig Marié <awenigmar@gmail.com>
2024-07-14 16:19:49 +0200

    Add cache for individual votes

--- application/controllers/Votes.php
+++ application/controllers/Votes.php
@@ -357,6 +357,11 @@
         show_404($this->functions_datan->get_404_infos());
       }
 
+      // Caching
+      if(!in_array($_SERVER['REMOTE_ADDR'], localhost()) && !$this->session->userdata('logged_in')){
+        $this->output->cache("4320"); // Caching enable for 3 days (1440 minutes per day)
+      }
+
       $data['legislature'] = $legislature;
 
       // Get vote
