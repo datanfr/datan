5c8df27afa5bd2185ca9386dab5cd3c87a0dcae3
Clem-Coding <clementine.blondeau@gmail.com>
2025-03-27 15:23:41 +0100

    add RANK() to get_mps_participation_solennels () method

--- application/models/Stats_model.php
+++ application/models/Stats_model.php
@@ -252,23 +252,33 @@
         return $this->db->query($sql, legislature_current())->result_array();
     }
   
-    public function get_mps_participation_solennels($legislature){
-      $sql = 'SELECT cp.*, da.nameFirst, da.nameLast, da.civ, da.libelle AS libelle, da.libelleAbrev AS libelleAbrev, da.dptSlug, da.nameUrl, da.couleurAssociee, da.img,
-        CONCAT(da.departementNom, " (", da.departementCode, ")") AS cardCenter, da.legislature AS legislature_last
-        FROM class_participation_solennels cp
-        LEFT JOIN deputes_last da ON cp.mpId = da.mpId AND da.legislature = cp.legislature
-        WHERE da.active AND cp.legislature = ?
-        ORDER BY cp.score DESC, cp.votesN DESC
-      ';
-      $array = $this->db->query($sql, $legislature)->result_array();
-      $i = 1;
-      foreach ($array as $key => $value) {
-        $array[$key]["rank"] = $i;
-        $i++;
-      }
-      return $array;
+    public function get_mps_participation_solennels($legislature): array
+    {
+        $sql = 'SELECT 
+                    RANK() OVER (ORDER BY cp.score DESC, cp.votesN DESC) AS "rank",
+                    cp.*, 
+                    da.nameFirst, 
+                    da.nameLast, 
+                    da.civ, 
+                    da.libelle AS libelle, 
+                    da.libelleAbrev AS libelleAbrev, 
+                    da.dptSlug, 
+                    da.nameUrl, 
+                    da.couleurAssociee, 
+                    da.img, 
+                    CONCAT(da.departementNom, " (", da.departementCode, ")") AS cardCenter, 
+                    da.legislature AS legislature_last
+                FROM class_participation_solennels cp
+                LEFT JOIN deputes_last da 
+                    ON cp.mpId = da.mpId 
+                    AND da.legislature = cp.legislature
+                WHERE da.active 
+                    AND cp.legislature = ?
+                ORDER BY cp.score DESC, cp.votesN DESC';
+    
+        return $this->db->query($sql, $legislature)->result_array();
     }
-
+    
     public function get_mps_participation_commission($legislature): array
     {
     $sql = 'SELECT 
