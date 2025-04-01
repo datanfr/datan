d85e274238b6985850af5094bc9247485c094190
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-01 16:20:44 +0200

    Fix card "Nos autres statistiques": keep links fixed in list + minor CSS tweaks

--- application/views/classements/templates/footer.php
+++ application/views/classements/templates/footer.php
@@ -1,56 +1,39 @@
-    <div class="col-lg-3">
+<div class="col-lg-3">
       <div class="card card-right-posts sticky-top sticky-offset sticky-top-lg mt-5 mt-lg-0">
         <div class="card-header">
           Nos autres statistiques
         </div>
-        <ul class="list-group list-group-flush">
-          <?php if ($page != "deputes-age"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/deputes-age" class="no-decoration underline">L'âge des députés</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "groupes-age"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/groupes-age" class="no-decoration underline">L'âge moyen au sein des groupes</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "groupes-feminisation"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/groupes-feminisation" class="no-decoration underline">Le taux de féminisation des groupes</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "deputes-loyaute"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/deputes-loyaute" class="no-decoration underline">La proximité au groupe</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "groupes-cohesion"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/groupes-cohesion" class="no-decoration underline">La cohésion des groupes</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "deputes-participation"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/deputes-participation" class="no-decoration underline">La participation des députés</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "groupes-participation"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/groupes-participation" class="no-decoration underline">La participation des groupes</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "deputes-origine-sociale"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/deputes-origine-sociale" class="no-decoration underline">L'origine sociale des députés</a>
-            </li>
-          <?php endif; ?>
-          <?php if ($page != "groupes-origine-sociale"): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>statistiques/groupes-origine-sociale" class="no-decoration underline">La représentativité des groupes</a>
-            </li>
-          <?php endif; ?>
-        </ul>
+          <ul class="list-group list-group-flush">
+        <li class="list-group-item <?= ($page == 'deputes-age') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/deputes-age" class="no-decoration">L'âge des députés</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'groupes-age') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/groupes-age" class="no-decoration">L'âge moyen au sein des groupes</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'groupes-feminisation') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/groupes-feminisation" class="no-decoration">Le taux de féminisation des groupes</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'deputes-loyaute') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/deputes-loyaute" class="no-decoration">La proximité au groupe</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'groupes-cohesion') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/groupes-cohesion" class="no-decoration">La cohésion des groupes</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'deputes-participation') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/deputes-participation" class="no-decoration">La participation des députés</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'groupes-participation') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/groupes-participation" class="no-decoration">La participation des groupes</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'deputes-origine-sociale') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/deputes-origine-sociale" class="no-decoration">L'origine sociale des députés</a>
+        </li>
+        <li class="list-group-item <?= ($page == 'groupes-origine-sociale') ? 'active' : '' ?>">
+          <a href="<?= base_url() ?>statistiques/groupes-origine-sociale" class="no-decoration">La représentativité des groupes</a>
+        </li>
+      </ul>
       </div>
     </div>
   </div>
 </div>
+
