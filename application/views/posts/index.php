1c9c7e6403c98f7428c81f7055717217dfdc5b36
Awenig Marié <awenigmar@gmail.com>
2025-04-02 23:48:23 +0200

    Redesign datan.fr/blog page

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,62 +1,38 @@
-<div class="container-fluid bloc-img-deputes bloc-img d-flex async_background" id="container-always-fluid" style="height: 14em">
-  <div class="container d-flex flex-column justify-content-center py-2">
-    <div class="row">
-      <div class="col-12">
+<div class="container my-5 pg-posts">
+  <div class="row">
+    <!-- Sticky Sidebar -->
+    <div class="menu col-md-3">
+      <div class="sticky-top" style="top: 80px;">
         <h1><?= $title ?></h1>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="container my-3 pg-posts">
-  <div class="row row-grid">
-    <div class="col-md-9">
-      <?php foreach ($posts as $post): ?>
-        <div class="card card-post mt-3">
-          <div class="row no-gutters">
-            <div class="col-auto img-wrap d-none d-lg-block">
-              <picture>
-                <source srcset="<?= asset_url() ?>imgs/posts/webp/img_post_<?= $post['id'] ?>.webp" type="image/webp">
-                <source srcset="<?= asset_url() ?>imgs/posts/img_post_<?= $post['id'] ?>.png" type="image/png">
-                <img src="<?= asset_url() ?>imgs/posts/img_post_<?= $post['id'] ?>.png" alt="Image post">
-              </picture>
-            </div>
-            <div class="col">
-              <div class="card-block p-3">
-                <span class="category mr-2"><?= mb_strtoupper($post['category_name']) ?></span>
-                <span class="date mr-2"><?= $post['created_at_fr'] ?></span>
-                <h2 class="card-title">
-                  <a href="<?= base_url() ?>blog/<?= $post['category_slug'] ?>/<?= $post['slug'] ?>" class="stretched-link">
-                    <?= $post['title'] ?>
-                  </a>
-                </h2>
-                <p class="card-text"><?= word_limiter(Strip_tags($post['body']), 25) ?></p>
-                <?php if ($user == "admin" || $user == "writer"): ?>
-                  <?php if ($post['state'] == "published"): ?>
-                    <span class="badge badge-success">En ligne</span>
-                    <?php else: ?>
-                    <span class="badge badge-danger">Brouillon</span>
-                  <?php endif; ?>
-                <?php endif; ?>
-              </div>
-            </div>
-          </div>
-        </div>
-      <?php endforeach; ?>
-    </div>
-    <div class="col-md-3">
-      <div class="card card-right-posts mt-lg-3">
-        <div class="card-header">
-          Catégories
-        </div>
-        <ul class="list-group list-group-flush">
-          <?php foreach ($categories as $category): ?>
-            <li class="list-group-item">
-              <a href="<?= base_url() ?>blog/categorie/<?= $category['slug'] ?>" class="no-decoration underline"><?= $category['name'] ?></a>
+        <h3 class="mt-4">Nos analyses et décryptages sur l'Assemblée nationale et les députés</h3>
+        <div class="mt-4 categories">
+          <ul class="list-unstyled mt-4">
+            <li>
+              <a href="<?= base_url() ?>blog" class="no-decoration underline">Derniers articles</a>
             </li>
-          <?php endforeach; ?>
-        </ul>
+            <li>
+              <a href="<?= base_url() ?>blog/categorie/datan" class="no-decoration underline">Datan</a>
+            </li>
+            <li>
+              <a href="<?= base_url() ?>blog/categorie/actualite-politique" class="no-decoration underline">Actualités politiques</a>
+            </li>
+            <li>
+              <a href="<?= base_url() ?>blog/categorie/rapports" class="no-decoration underline">Rapports</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <?php $this->load->view('posts/partials/follow_us.php') ?>
+    </div>
+
+    <!-- Main Content -->
+    <div class="col-md-9 pl-5">
+      <div class="row">
+        <?php foreach ($posts as $post): ?>
+          <div class="col-6 mb-5">
+            <?php $this->load->view('posts/partials/bloc-post.php', array('post' => $post, 'chapo' => TRUE)) ?>            
+          </div>
+        <?php endforeach; ?>
+      </div>
     </div>
   </div>
 </div>
