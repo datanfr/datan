ef60b0e5a8934384789efb9e249980360c933c8f
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-10 17:57:43 +0200

    Work on iframe depute page

--- application/libraries/ElectionService.php
+++ application/libraries/ElectionService.php
@@ -11,12 +11,11 @@ class ElectionService
     public function get_all_elections(string $mp_id, array $gender): array
     {
         $elections = $this->CI->elections_model->get_candidate_elections($mp_id, TRUE, TRUE);
-        
+
         foreach ($elections as $key => $value) {
-    
+
             $elections[$key]['district'] = $this->CI->elections_model->get_district($value['libelleAbrev'], $value['district']);
             
-    
             if ($value['elected'] === "1") {
                 $elections[$key]['electedLibelle'] = 'Élu' . $gender['e'];
                 $elections[$key]['electedColor'] = 'adopté';
