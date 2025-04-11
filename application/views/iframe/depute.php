5a8810219428b5c64be2aeccdcef94de17cb9957
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-11 15:59:46 +0200

    depute iframe prototype

--- application/views/iframe/depute.php
+++ application/views/iframe/depute.php
@@ -1,18 +1,18 @@
-<?php
-    if ($category === 'positions-importantes') {
-        $this->view('deputes/partials/mp_individual/_key_positions.php');
-    }
-
-    if ($category === 'derniers-votes') {
-        $this->view('deputes/partials/mp_individual/_votes.php');
-    }
-
-    if ($category === 'election') {
-        $this->view('deputes/partials/mp_individual/_election.php');
-    }
-
-    if ($category === 'comportement-politique') {
-        $this->view('deputes/partials/mp_individual/statistics/_index.php');
-    }
-?>
+<div class="container pg-depute-individual">
+  <div class="row">
+    <div class="col-md-10 col-lg-8 offset-md-1 offset-lg-0 pl-lg-5">
+      <?php
+        if ($category === 'positions-importantes') {
+            $this->view('deputes/partials/mp_individual/_key_positions.php');
+        } elseif ($category === 'derniers-votes') {
+            $this->view('deputes/partials/mp_individual/_votes.php');
+        } elseif ($category === 'election') {
+            $this->view('deputes/partials/mp_individual/_election.php');
+        } elseif ($category === 'comportement-politique') {
+            $this->view('deputes/partials/mp_individual/statistics/_index.php');
+        }
+      ?>
+    </div>
+  </div>
+</div>
 
