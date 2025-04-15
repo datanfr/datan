0a9c5e7fbaab8dd5c257e50bac52eb61a54918c9
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-15 10:02:15 +0200

    Apply PR review changes

--- application/libraries/Depute_service.php
+++ application/libraries/Depute_service.php
@@ -1,6 +1,6 @@
 <?php
 
-class DeputeService 
+class Depute_service 
 {
 
     public function __construct() 
@@ -101,21 +101,7 @@ class DeputeService
       }
 
 
-    public function get_group_info(array $data, string $mp_id, string $groupe_id): array 
-    {
-        if (!empty($data['depute']['libelle'])) {
-            $data['depute']['couleurAssociee'] = $this->CI->groupes_model->get_groupe_color(array($data['depute']['libelleAbrev'], $data['depute']['couleurAssociee']));
-            
-            $data['group_president'] = $this->CI->deputes_model->depute_group_president($mp_id, $groupe_id);
-            $data['isGroupPresident'] = !empty($data['group_president']) ? TRUE : FALSE;
-        } else {
-            $groupe_id = NULL;
-            $data['isGroupPresident'] = FALSE;  
-        }
-
-    return $data;  
-    }
-
+   
 
     public function get_general_infos(
         array $data,
