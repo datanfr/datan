a4e1ae7879448a432b527eeb47364547415c6b34
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-23 18:06:46 +0200

    change logic for url

--- application/views/iframe/depute.php
+++ application/views/iframe/depute.php
@@ -4,27 +4,26 @@
       Les données parlementaires et électorales de <?= $mp_full_name; ?>
     </h1>
   <?php endif; ?>
+
   <?php
-
-foreach ($views_to_load as $view): ?>
-  <?php if (strpos($view, 'statistics') !== false && !$title_displayed && $has_comportement_subcategories): ?>
-    <div class="bloc-statistiques mt-5">
-      <?php if (!isset($iframe_title_visibility) || $iframe_title_visibility !== 'hidden'): ?>
-        <h2 class="mb-3 title-center">
-          Son comportement politique
-          <?php if ($depute['legislature'] != legislature_current()): ?>
-            (<?= $depute['legislature'] ?><sup>e</sup> législature)
-          <?php endif; ?>
-        </h2>
+  foreach ($views_to_load as $view): ?>
+    <?php if (strpos($view, 'statistics') !== false && !$title_displayed && $has_comportement_subcategories): ?>
+      <div class="bloc-statistiques mt-5">
+        <?php if (!isset($iframe_title_visibility) || $iframe_title_visibility !== 'hidden'): ?>
+          <h2 class="mb-3 title-center">
+            Son comportement politique
+            <?php if ($depute['legislature'] != legislature_current()): ?>
+              (<?= $depute['legislature'] ?><sup>e</sup> législature)
+            <?php endif; ?>
+          </h2>
+        <?php endif; ?>
+        <?php $title_displayed = true; ?>
       <?php endif; ?>
-    </div>
-    <?php $title_displayed = true; ?>
-  <?php endif; ?>
-
-  <?php $this->view($view); ?>
-<?php endforeach; ?>
-
-
+      <?php $this->view($view); ?>
+      <?php if (strpos($view, 'statistics') !== false && !$title_displayed && $has_comportement_subcategories): ?>
+      </div>
+    <?php endif; ?>
+  <?php endforeach; ?>
 
   <p class="datan-credit">
     <img src="/assets/imgs/favicon/datan_favicon.svg" alt="logo de Datan" class="datan-logo">
