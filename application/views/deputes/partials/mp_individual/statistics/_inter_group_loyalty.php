47fb12a0680098220e693afe53213f118bd5d856
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-29 17:23:44 +0200

    completed 1st/3rd person phrasing logic

--- application/views/deputes/partials/mp_individual/statistics/_inter_group_loyalty.php
+++ application/views/deputes/partials/mp_individual/statistics/_inter_group_loyalty.php
@@ -1,54 +1,78 @@
-<!-- CARD MAJORITE -->
-<?php if (!in_array($depute['groupeId'], $this->groupes_model->get_all_groupes_majority()) && $depute['legislature'] != 17): ?>
-  <div class="card card-statistiques my-4">
-    <div class="card-body">
-      <div class="row">
-        <div class="col-12 d-flex flex-row align-items-center">
-          <div class="icon">
-            <?= file_get_contents(base_url() . '/assets/imgs/icons/elysee.svg') ?>
-          </div>
-          <h3 class="ml-3 text-uppercase">Proximité avec la majorité gouvernementale
-            <a tabindex="0" role="button" data-toggle="popover" class="no-decoration popover_focus" data-trigger="focus" aria-label="Tooltip majorité" title="Proximité avec la majorité gouvernementale" data-content="Le <b>taux de proximité avec la majorité gouvernementale</b> représente le pourcentage de fois où un député vote la même chose que le groupe présidentiel (La République en Marche).<br><br>Pour plus d'information, <a href='<?= base_url() ?>statistiques/aide#proximity' target='_blank'>cliquez ici</a>." id="popover_focus"><?= file_get_contents(asset_url() . "imgs/icons/question_circle.svg") ?></a>
-          </h3>
-        </div>
-      </div>
-      <div class="row">
-        <?php if ($no_majorite) : ?>
-          <div class="col-12 mt-2">
-            <p>Cette statistique sera disponible dès qu'un nombre suffisant de votes sera atteint pour <?= $title ?>.</p>
-          </div>
-        <?php else : ?>
-          <div class="col-lg-3 offset-lg-1 mt-2">
-            <div class="d-flex justify-content-center align-items-center">
-              <div class="c100 p<?= $majorite['score'] ?> m-0">
-                <span><?= $majorite['score'] ?> %</span>
-                <div class="slice">
-                  <div class="bar"></div>
-                  <div class="fill"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-8 infos mt-4 mt-lg-2">
-            <div class="texte ml-md-3 pl-md-3 mt-md-0 mt-3">
-              <p>
-                <?= $title ?> a voté comme la majorité gouvernementale dans <?= $majorite['score'] ?>% des cas. Les votes de <?= $title ?> sont comparés à ceux du groupe politique le plus gros de la majorité (<a href="<?= base_url() ?>groupes/legislature-<?= $groupMajority['legislature'] ?>/<?= mb_strtolower($groupMajority['libelleAbrev']) ?>"><?= name_group($groupMajority['libelle']) ?></a>).
-              </p>
-              <p>
-                <?= ucfirst($gender['pronom']) ?> <?= $active ? "est" : "était" ?> <b><?= $edito_majorite['all'] ?></b> de la majorité gouvernementale que la moyenne des députés non membres de la majorité (<?= $majorite['all'] ?>%).
-              </p>
-              <?php if ($majorite['group']): ?>
-                <p>
-                  De plus, <?= $title ?> <?= $active ? "est" : "était" ?> <b><?= $edito_majorite['group'] ?></b> de la majorité gouvernementale que la moyenne des députés de son groupe politique (<?= $majorite['group'] ?>%).
-                </p>
-              <?php endif; ?>
-            </div>
-          </div>
-        <?php endif; ?>
-      </div>
-    </div>
-  </div>
-<?php endif; ?>
+<!-- // CARD GROUPS PROXIMITY -->
+
+<?php
+$first_person = false;
+$active = true;
+
+if ($first_person && $active) {
+  $first_section_title = "Je vote <b>souvent</b> avec :";
+  $first_section_content = "";
+  if ($depute['libelleAbrev'] != 'NI') {
+    $first_section_content .= "En plus de mon propre groupe, je ";
+  } else {
+    $first_section_content .= "Je ";
+  }
+  if (isset($proximite)) {
+    $first_section_content .= "vote souvent (dans {$proximite['first1']['accord']}% des cas) avec le groupe " .
+      "<a href='" . base_url() . "groupes/legislature-{$depute['legislature']}/" .
+      mb_strtolower($proximite['first1']['libelleAbrev']) . "'>{$proximite['first1']['libelleAbrev']}</a>, " .
+      "{$proximite['first1']['maj_pres']}";
+
+    if ($proximite['first1']['libelleAbrev'] != 'NI') {
+      $first_section_content .= " classé {$proximite['first1']['ideologiePolitique']['edited']} de l'échiquier politique.";
+    }
+
+    $second_section_title = "Je vote <b>rarement</b> avec :";
+
+    $second_section_content = "";
+
+    $second_section_content .= "À l'opposé, le groupe avec lequel je suis le moins proche est ";
+    $second_section_content .= "<a href='" . base_url() . "groupes/legislature-{$depute['legislature']}/" . mb_strtolower($proximite['last1']['libelleAbrev']) . "'>{$proximite['last1']['libelle']}</a>, ";
+    $second_section_content .= "{$proximite['last1']['maj_pres']}";
+
+    if ($proximite['last1']['libelleAbrev'] != "NI") {
+      $second_section_content .= " classé {$proximite['last1']['ideologiePolitique']['edited']} de l'échiquier politique.";
+    }
+
+    $second_section_content .= " Je ne vote avec ce groupe que dans <b>{$proximite['last1']['accord']}%</b> des cas.";
+  }
+} else {
+  $first_section_title = $title . " " . ($depute['legislature'] == legislature_current() ? "vote" : "votait") . " <b>souvent</b> avec :";
+
+  $first_section_content = "";
+
+  if ($depute['libelleAbrev'] != "NI") {
+    $first_section_content .= "En plus de son propre groupe, ";
+  }
+  if (isset($proximite)) {
+
+    $first_section_content .= "<b>{$title}</b> " . ($active ? "vote" : "votait") . " souvent (dans {$proximite['first1']['accord']}% des cas) avec le groupe ";
+    $first_section_content .= "<a href='" . base_url() . "groupes/legislature-{$depute['legislature']}/" . mb_strtolower($proximite['first1']['libelleAbrev']) . "'>{$proximite['first1']['libelleAbrev']}</a>, ";
+    $first_section_content .= "{$proximite['first1']['maj_pres']}";
+
+    if ($proximite['first1']['libelleAbrev'] != "NI") {
+      $first_section_content .= " classé {$proximite['first1']['ideologiePolitique']['edited']} de l'échiquier politique.";
+    }
+
+    $second_section_title = ucfirst($gender['pronom']) . ' ' .
+      ($depute['legislature'] == legislature_current() ? 'vote' : 'votait') . ' <b>rarement</b> avec :';
+
+    $second_section_content = "";
+    $second_section_content .= "À l'opposé, le groupe avec lequel {$title} " . ($active ? "est" : "était") . " le moins proche est ";
+    $second_section_content .= "<a href='" . base_url() . "groupes/legislature-{$depute['legislature']}/" . mb_strtolower($proximite['last1']['libelleAbrev']) . "'>{$proximite['last1']['libelle']}</a>, ";
+    $second_section_content .= "{$proximite['last1']['maj_pres']}";
+
+    if ($proximite['last1']['libelleAbrev'] != "NI") {
+      $second_section_content .= " classé {$proximite['last1']['ideologiePolitique']['edited']} de l'échiquier politique.";
+    }
+
+    $second_section_content .= " " . ucfirst($gender["pronom"]) . " " . ($active ? "ne vote" : "n'a voté") . " avec ce groupe que dans <b>{$proximite['last1']['accord']}%</b> des cas.";
+  }
+}
+?>
+
+
+
 <div class="card card-statistiques bloc-proximity my-4">
   <div class="card-body">
     <div class="row">
@@ -56,15 +80,36 @@
         <div class="icon">
           <?= file_get_contents(base_url() . '/assets/imgs/icons/group.svg') ?>
         </div>
-        <h3 class="ml-3 text-uppercase">Proximité avec les groupes politiques
-          <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" aria-label="Tooltip proximité" class="no-decoration popover_focus" title="Proximité avec les groupes politiques" data-content="Le <b>taux de proximité avec les groupes</b> représente le pourcentage de fois où un député vote la même chose qu'un groupe parlementaire. Chaque groupe se voit attribuer pour chaque vote une <i>position majoritaire</i>, en fonction du vote de ses membres. Cette position peut soit être 'pour', 'contre', ou 'absention'. Pour chaque vote, nous déterminons si le ou la députée a voté la même chose que la position majoritaire d'un groupe. Le taux de proximité est le pourcentage de fois où le ou la députée a voté de la même façon qu'un groupe.<br><br>Par exemple, si le taux est de 75%, cela signifie que <?= $title ?> a voté avec ce groupe dans 75% des cas.<br><br>Pour plus d'information, <a href='<?= base_url() ?>statistiques/aide#proximity' target='_blank'>cliquez ici</a>." id="popover_focus"><?= file_get_contents(asset_url() . "imgs/icons/question_circle.svg") ?></a>
+        <h3 class="ml-3 text-uppercase">
+          Proximité avec les groupes politiques
+          <a
+            tabindex="0"
+            role="button"
+            data-toggle="popover"
+            data-trigger="focus"
+            aria-label="Tooltip proximité"
+            class="no-decoration popover_focus"
+            title="Proximité avec les groupes politiques"
+            data-content="Le <b>taux de proximité avec les groupes</b> représente le pourcentage de fois où un député 
+            vote de la même manière qu'un groupe parlementaire. Chaque groupe se voit attribuer pour chaque vote 
+            une <i>position majoritaire</i>, en fonction du vote de ses membres. Cette position peut soit être 'pour',
+            'contre', ou 'absention'. Pour chaque vote, nous vérifions si le député a voté en accord avec la position 
+            majoritaire d'un groupe. Le taux de proximité correspond au pourcentage de votes où le député a voté conformément
+            à ce groupe.<br><br>Par exemple, si le taux est de 75%, cela signifie que <?= $title ?> 
+            a voté avec ce groupe dans 75% des cas.<br><br>Pour plus d'information, 
+            <a href='<?= base_url() ?>statistiques/aide#proximity' target='_blank'>cliquez ici</a>."
+            id="popover_focus">
+            <?= file_get_contents(asset_url() . "imgs/icons/question_circle.svg") ?>
+          </a>
         </h3>
+
       </div>
     </div>
     <?php if ($no_votes != TRUE) : ?>
       <div class="row">
         <div class="offset-2 col-10">
-          <h4><?= $title ?> <?= $depute['legislature'] == legislature_current() ? "vote" : "votait" ?> <b>souvent</b> avec :</h4>
+          <!-- // Première phrase-->
+          <h4><?= $first_section_title ?></h4>
         </div>
       </div>
       <div class="row mt-1 bar-container stats pr-2">
@@ -103,24 +148,14 @@
       <?php if ($depute['legislature'] == legislature_current() && dissolution() === false): ?>
         <div class="row mt-3">
           <div class="offset-2 col-10">
-
-            <p>
-              <?php if ($group['libelleAbrev'] != "NI") : ?>
-                En plus de son propre groupe,
-              <?php else : ?>
-                Le
-              <?php endif; ?>
-              <b><?= $title ?></b> <?= $active ? "vote" : "votait" ?> souvent (dans <?= $proximite["first1"]["accord"] ?>% des cas) avec le groupe <a href="<?= base_url() ?>groupes/legislature-<?= $depute["legislature"] ?>/<?= mb_strtolower($proximite["first1"]["libelleAbrev"]) ?>"><?= $proximite["first1"]["libelleAbrev"] ?></a>, <?= $proximite["first1"]["maj_pres"] ?>
-              <?php if ($proximite["first1"]["libelleAbrev"] != "NI") : ?>
-                classé <?= $proximite["first1"]["ideologiePolitique"]["edited"] ?> de l'échiquier politique.
-              <?php endif; ?>
-            </p>
+            <!-- 1ère section content-->
+            <p> <?= $first_section_content ?></p>
           </div>
         </div>
       <?php endif; ?>
       <div class="row mt-5">
         <div class="col-10 offset-2">
-          <h4><?= ucfirst($gender['pronom']) ?> <?= $depute['legislature'] == legislature_current() ? "vote" : "votait" ?> <b>rarement</b> avec :</h4>
+          <h4><?= $second_section_title ?></h4>
         </div>
       </div>
       <div class="row mt-1 bar-container stats pr-2">
@@ -159,14 +194,8 @@
       <?php if ($depute['legislature'] == legislature_current() && dissolution() === false): ?>
         <div class="row mt-3">
           <div class="col-10 offset-2 ">
-
-            <p>
-              À l'opposé, le groupe avec lequel <?= $title; ?> <?= $active ? "est" : "était" ?> le moins proche est <a href="<?= base_url() ?>groupes/legislature-<?= $depute["legislature"] ?>/<?= mb_strtolower($proximite["last1"]["libelleAbrev"]) ?>"><?= $proximite["last1"]["libelle"] ?></a>, <?= $proximite["last1"]["maj_pres"] ?>
-              <?php if ($proximite["last1"]["libelleAbrev"] != "NI") : ?>
-                classé <?= $proximite["last1"]["ideologiePolitique"]["edited"] ?> de l'échiquier politique.
-              <?php endif; ?>
-              <?= ucfirst($gender["pronom"]) ?> <?= $active ? "ne vote" : "n'a voté" ?> avec ce groupe que dans <b><?= $proximite["last1"]["accord"] ?>%</b> des cas.
-            </p>
+            <!-- 2nd section content-->
+            <p><?= $second_section_content ?></p>
           </div>
         </div>
       <?php endif; ?>
@@ -209,4 +238,4 @@
       <p>Cette statistique sera disponible dès qu'un nombre suffisant de votes sera atteint pour <?= $title ?>.</p>
     <?php endif; ?>
   </div>
-</div> <!-- // END BLOC PROXIMITY -->
+</div> <!-- // END BLOC GROUPS PROXIMITY -->
--- application/views/deputes/partials/mp_individual/statistics/_voting_participation.php
+++ application/views/deputes/partials/mp_individual/statistics/_voting_participation.php
@@ -1,3 +1,46 @@
+<?php
+if ($first_person && $active) {
+
+  if (!empty($participation['score'])) {
+    $first_phrase = "Depuis ma prise de fonctions, j'ai participé à {$participation['score']}% des votes solennels à l'Assemblée nationale.";
+  }
+
+  if (!empty($edito_participation['all']) && !empty($participation['all'])) {
+    $second_phrase = "Je vote <b>{$edito_participation['all']}</b> que la moyenne des députés, qui est de " .
+      ($edito_participation['all'] == 'autant' ? 'également ' : '') . "{$participation['all']}%.";
+  }
+
+  if (!empty($edito_participation['group']) && !empty($participation['group'])) {
+    $third_phrase = "De plus, je vote <b>{$edito_participation['group']}</b> que la moyenne des députés de mon groupe politique, qui est de " .
+      ($edito_participation['group'] == 'autant' ? 'également ' : '') . "{$participation['group']}%.";
+  }
+} else {
+
+  if (!empty($participation['score'])) {
+    $first_phrase = $active
+      ? "Depuis sa prise de fonctions, {$title} a participé à {$participation['score']}% des votes solennels à l'Assemblée nationale."
+      : "Quand {$gender['pronom']} était en activité à l'Assemblée, {$title} a participé à {$participation['score']}% 
+        des votes solennels à l'Assemblée nationale.";
+  }
+
+  if (!empty($gender['pronom']) && !empty($edito_participation['all']) && !empty($participation['all'])) {
+    $second_phrase = $active
+      ? ucfirst($gender['pronom']) . " vote <b>{$edito_participation['all']}</b> que la moyenne des députés, " .
+      ($edito_participation['all'] == 'autant' ? 'également ' : '') . "qui est de {$participation['all']}%."
+      : ucfirst($gender['pronom']) . " votait <b>{$edito_participation['all']}</b> que la moyenne des députés, " .
+      ($edito_participation['all'] == 'autant' ? 'également ' : '') . "qui est de {$participation['all']}%.";
+  }
+
+  if (!empty($edito_participation['group']) && !empty($participation['group'])) {
+    $third_phrase = $active
+      ? "De plus, {$title} vote <b>{$edito_participation['group']}</b> que la moyenne des députés de son groupe politique, qui est de " .
+      ($edito_participation['group'] == 'autant' ? 'également ' : '') . "{$participation['group']}%."
+      : "De plus, {$title} votait <b>{$edito_participation['group']}</b> que la moyenne des députés de son groupe politique, qui est de " .
+      ($edito_participation['group'] == 'autant' ? 'également ' : '') . "{$participation['group']}%.";
+  }
+}
+?>
+
 <div class="card card-statistiques my-4">
   <div class="card-body">
     <div class="row">
@@ -5,15 +48,37 @@
         <div class="icon">
           <?= file_get_contents(base_url() . '/assets/imgs/icons/voting.svg') ?>
         </div>
-        <h3 class="ml-3 text-uppercase">Participation aux votes
-          <a tabindex="0" role="button" data-toggle="popover" data-trigger="focus" aria-label="Tooltip participation" class="no-decoration popover_focus" title="Taux de participation" data-content="Le taux de participation est le <b>pourcentage de votes auxquels le ou la député a participé</b>.<br><br>Attention, le taux de participation ne mesure pas toute l'activité d'un député ou d'un groupe. Contrairement au <a href='https://www.europarl.europa.eu/about-parliament/fr/organisation-and-rules/how-plenary-works' title='lien'>Parlement européen</a>, les votes à l'Assemblée nationale se déroulent à n'importe quel moment de la semaine. D'autres réunions ont souvent lieu en même temps, expliquant le faible taux de participation des députés et des groupes.<br><br>Pour plus d'information, <a href='<?= base_url() ?>statistiques/aide#participation' target='_blank'>cliquez ici</a>."><?= file_get_contents(asset_url() . "imgs/icons/question_circle.svg") ?></a>
+        <h3 class="ml-3 text-uppercase">
+          Participation aux votes
+          <a
+            tabindex="0"
+            role="button"
+            data-toggle="popover"
+            data-trigger="focus"
+            aria-label="Tooltip participation"
+            class="no-decoration popover_focus"
+            title="Taux de participation"
+            data-content="Le taux de participation est le <b>pourcentage de votes auxquels le député ou la
+        députée a participé</b>.<br><br>Attention, le taux de participation ne mesure pas toute
+        l'activité d'un député ou d'un groupe. Contrairement au 
+        <a href='https://www.europarl.europa.eu/about-parliament/fr/organisation-and-rules/how-plenary-works' 
+        title='lien'>Parlement européen</a>, les votes à l'Assemblée nationale se déroulent à n'importe quel moment de la semaine.
+        D'autres réunions ont souvent lieu en même temps, expliquant le faible taux de
+        participation des députés et des groupes.<br><br>Pour plus d'information,
+        <a href='<?= base_url() ?>statistiques/aide#participation' target='_blank'>cliquez ici</a>.">
+            <?= file_get_contents(asset_url() . "imgs/icons/question_circle.svg") ?>
+          </a>
         </h3>
+
       </div>
     </div>
     <div class="row">
       <?php if ($no_participation) : ?>
         <div class="col-12 mt-2">
-          <p>Il n'y a pas encore eu suffisamment de votes solennels à l'Assemblée nationale pour afficher cette statistique. Pour consulter la participation des députés à l'ensemble des scrutins, <a href="<?= base_url() ?>statistiques/deputes-participation">cliquez ici</a>.</p>
+          <p>Il n'y a pas encore eu suffisamment de votes solennels à l'Assemblée nationale pour afficher cette statistique.
+            Pour consulter la participation des députés à l'ensemble des scrutins,
+            <a href="<?= base_url() ?>statistiques/deputes-participation">cliquez ici</a>.
+          </p>
         </div>
       <?php else : ?>
         <div class="col-lg-3 offset-lg-1 mt-2">
@@ -30,40 +95,29 @@
         <div class="col-lg-8 infos mt-4 mt-lg-2">
           <div class="texte ml-md-3 pl-md-3 mt-md-0 mt-3">
             <?php if ($depute['legislature'] == legislature_current()): ?>
-              <!-- Paragraph for MP from the current legislature -->
-              <p>
-                <?php if ($active) : ?>
-                  Depuis sa prise de fonctions,
-                <?php else : ?>
-                  Quand <?= $gender['pronom'] ?> était en activité à l'Assemblée,
-                <?php endif; ?>
-                <?= $title ?> a participé à <?= $participation['score'] ?>% des votes solennels à l'Assemblée nationale.
-              </p>
-              <p>
-                <?= ucfirst($gender['pronom']) ?> <?= $active ? "vote" : "votait" ?> <b><?= $edito_participation['all'] ?></b> que la moyenne des députés, qui est <?= $edito_participation['all'] == "autant" ? "également" : "" ?> de <?= $participation['all'] ?>%.
-              </p>
-              <?php if ($participation['group']): ?>
-                <p>
-                  De plus, <?= $title ?> <?= $active ? "vote" : "votait" ?> <b><?= $edito_participation['group'] ?></b> que la moyenne des députés de son groupe politique, qui est <?= $edito_participation['group'] == "autant" ? "également" : "" ?> de <?= $participation['group'] ?>%.
-                </p>
-              <?php endif; ?>
+              <p><?= $first_phrase ?></p>
+              <p><?= $second_phrase ?></p>
+              <p><?= $third_phrase ?></p>
             <?php else: ?>
               <!-- Paragraph for MP from older legislatures -->
               <p>
-                Pendant la <?= $depute['legislature'] ?><sup>e</sup> législature, <?= $title ?> a participé à <?= $participation['score'] ?>% des votes solennels à l'Assemblée nationale.
+                Pendant la <?= $depute['legislature'] ?><sup>e</sup> législature, <?= $title ?> a participé à
+                <?= $participation['score'] ?>% des votes solennels à l'Assemblée nationale.
               </p>
               <p>
-                <?= ucfirst($gender['pronom']) ?> <?= $active ? "vote" : "votait" ?> <b><?= $edito_participation['all'] ?></b> que la moyenne des députés, qui était <?= $edito_participation['all'] == "autant" ? "également" : "" ?> de <?= $participation['all'] ?>%.
+                <?= ucfirst($gender['pronom']) ?> votait <b><?= $edito_participation['all'] ?></b> que la moyenne des
+                députés, qui était <?= $edito_participation['all'] == "autant" ? "également" : "" ?> de <?= $participation['all'] ?>%.
               </p>
               <?php if ($participation['group']): ?>
                 <p>
-                  De plus, <?= $title ?> <?= $active ? "vote" : "votait" ?> <b><?= $edito_participation['group'] ?></b> que la moyenne des députés de son groupe politique, qui était <?= $edito_participation['group'] == "autant" ? "également" : "" ?> de <?= $participation['group'] ?>%.
+                  De plus, <?= $title ?> votait <b><?= $edito_participation['group'] ?></b> que la moyenne des députés
+                  de son groupe politique, qui était <?= $edito_participation['group'] == "autant" ? "également" : "" ?>
+                  de <?= $participation['group'] ?>%.
                 </p>
               <?php endif; ?>
             <?php endif; ?>
-            <p>
-              Les votes solennels sont les votes considérés comme importants pour lesquels les députés connaissent à l'avance le jour et l'heure du vote.
-            </p>
+            <p>Les votes solennels sont les votes considérés comme importants pour lesquels les députés connaissent
+              à l'avance le jour et l'heure du vote.</p>
           </div>
         </div>
       <?php endif; ?>
