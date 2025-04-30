2f47950dcdd26b3053c5ab50c2d3d915f2c95fd6
Clem-Coding <clementine.blondeau@gmail.com>
2025-04-30 13:52:30 +0200

    person mode for main title and key positions

--- application/views/deputes/partials/mp_individual/_key_positions.php
+++ application/views/deputes/partials/mp_individual/_key_positions.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-12">
         <?php if (!isset($iframe_title_visibility) || $iframe_title_visibility !== 'hidden'): ?>
-          <h2 class="mb-4 title-center"><?=$first_person ? 'Mes' : 'Ses' ?> positions importantes</h2>
+          <h2 class="mb-4 title-center"><?= $first_person ? 'Mes' : 'Ses' ?> positions importantes</h2>
         <?php endif; ?>
         <div class="card">
           <div class="card-body key-votes">
@@ -14,18 +14,23 @@
                   <span class="sort-<?= $value["vote_libelle"] ?>"><?= mb_strtoupper($value["vote_libelle"]) ?></span>
                 </div>
                 <div class="col-md-9 value">
-                  <?= $title ?><b>
+                  <?= !$first_person ? $title : '' ?>
+                  <b>
                     <?php if ($value['vote'] === "1") : ?>
-                      a voté en faveur de
+                      <?= $first_person ? "J'ai voté en faveur de" : "a voté en faveur de" ?>
                     <?php elseif ($value['vote'] === "-1") : ?>
-                      a voté contre
+                      <?= $first_person ? "J'ai voté contre" : "a voté contre" ?>
                     <?php else : ?>
-                      s'est abstenu<?= $gender['e'] ?> sur le vote concernant
+                      <?= $first_person ? "Je me suis abstenu{$gender['e']} sur le vote concernant" : "s'est abstenu{$gender['e']} sur le vote concernant" ?>
                     <?php endif; ?>
-                    <?= $value['text'] ?></b>.
-                  <?= ucfirst($gender["pronom"]) ?> <?= $value["scoreLoyaute"] === "1" ? "a voté " : "n'a pas voté " ?>comme son groupe.
+                    <?= $value['text'] ?>
+                  </b>.
+                  <?= $first_person
+                    ? ($value["scoreLoyaute"] === "1" ? "J'ai voté" : "Je n'ai pas voté") . " comme mon groupe."
+                    : ucfirst($gender["pronom"]) . " " . ($value["scoreLoyaute"] === "1" ? "a voté" : "n'a pas voté") . " comme son groupe." ?>
                   <a href="<?= base_url() ?>votes/legislature-16/vote_<?= $value['voteNumero'] ?>" class="font-italic">Voir le vote</a>
                 </div>
+
               </div>
             <?php endforeach; ?>
           </div>
