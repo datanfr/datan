1d996c307c130b32ef8ae7b4e6fba1624be3671d
Marc Bernini <bernini.marc@gmail.com>
2025-06-27 18:02:47 +0200

    ✨ feat: Display custom campaign
    ✨ feat(Campaign_model.php): get_current_active
    
    Add function to select campaigns active and start today or after and ending after today
    
    ✨ feat(Campaign.php): New controller
    
    For Campaign methods. With first method "current_active_campaigns"
    
    ✨ feat(routes.php): New route campaign
    
    Get all actives campaigns
    
    ✨ feat(campaigns.js): Script for campaigns
    
    To get all active campaigns
    
    WIP
    
    ✨ feat(index.php): Include campaigns JS
    
    WIP
    
    🎈 perf(Campaign_model.php): Add order by
    
    Get latest updated_at first
    
    🦄 refactor(index.php): Remove campaign script
    
    Used in campaign.php
    
    ✨ feat(campaign.php, main.scss): Section to display campaign
    
    Hide by default. Use ids to display and inject html.
    Inspire styled by existing donation section. With specific scss
    
    ✨ feat(campaign.js): Complete JS
    
    Check if campaigns exist and inject html custom of first result
    
    🔧 build(main.css, main.css.map): Build css

---
+++ application/views/partials/campaign.php
@@ -0,0 +1,9 @@
+<section id="campaign" class="container-fluid d-none" >
+  <div class="container d-flex justify-content-center flex-column align-items-center py-4">
+      <div class="icon"><?= file_get_contents(asset_url() . 'imgs/icons/heart-fill.svg') ?></div>
+      <div id="campaign-paragraph" class="text-center my-4"></div>
+      <a class="btn btn-primary" href="https://www.helloasso.com/associations/datan/formulaires/1" target="_blank" rel="noopener">Faire un don</a>
+  </div>
+</section>
+
+<script src="<?= asset_url() ?>js/campaigns/campaigns.js" type="text/javascript"></script>
