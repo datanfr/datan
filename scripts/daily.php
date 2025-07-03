98fd2aae80209b000aa1b86eebe74a15ff60ddff
Awenig Marié <66798411+awenig@users.noreply.github.com>
2025-07-03 11:55:47 +0200

    Persistent connexion: false

--- scripts/daily.php
+++ scripts/daily.php
@@ -38,7 +38,7 @@ class Script
             $this->bdd = new PDO(
                 'mysql:host=' . $_SERVER['DATABASE_HOST'] . ';dbname=' . $_SERVER['DATABASE_NAME'], $_SERVER['DATABASE_USERNAME'], $_SERVER['DATABASE_PASSWORD'],
                 array(
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => false, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
                 )
             );
         } catch (Exception $e) {
