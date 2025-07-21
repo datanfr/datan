df55772dbd71915380ca05f950c5e029311eeac8
Clem-Coding <clementine.blondeau@gmail.com>
2025-07-21 20:18:57 +0200

    Add dev to about section

--- application/views/pages/a-propos.php
+++ application/views/pages/a-propos.php
@@ -308,7 +308,25 @@
         <div class="col-lg-3 col-md-4 col-6 mt-3 mt-md-0">
           <div class="card my-2 card-team">
             <div class="img-profile">
-              <img class="card-img-top" src="<?= asset_url() ?>imgs/team/philippe.jpg" alt="Photo Alexis">
+              <img class="card-img-top" src="<?= asset_url() ?>imgs/team/clementine.jpg" alt="Photo Clémentine">
+              <div class="overlay"></div>
+            </div>
+            <div class="card-body d-flex flex-column align-items-center">
+              <h3 class="card-title">Clémentine</h3>
+              <p class="card-text text-center">Développeuse</p>
+              <div class="social-media d-flex justify-content-center">
+                <a href="https://www.linkedin.com/in/cl%C3%A9mentine-blondeau-80a084332/" target="_blank">
+                  <img src="<?= asset_url() ?>imgs/logos/linkedin-round-color.svg" width="30" height="30" alt="Logo Linkedin">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Team 9 -->
+        <div class="col-lg-3 col-md-4 col-6 mt-3 mt-md-0">
+          <div class="card my-2 card-team">
+            <div class="img-profile">
+              <img class="card-img-top" src="<?= asset_url() ?>imgs/team/philippe.jpg" alt="Photo Philippe">
               <div class="overlay"></div>
             </div>
             <div class="card-body d-flex flex-column align-items-center">
