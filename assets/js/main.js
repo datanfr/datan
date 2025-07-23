653b25de86e6f6b58a0494be5aa68b39ffa45a35
Awenig Marié <awenigmar@gmail.com>
2025-07-23 22:24:22 +0200

    Change  main.js for webp cover hemicycle img

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,10 +140,19 @@ $('.no-img').css({ 'height': cw + 'px' });
 (() => {
   'use strict';
   // Page is loaded
+
+  // Check for WebP support 
+  const supportsWebP = (() => {
+    const elem = document.createElement('canvas');
+    return !!(elem.getContext && elem.getContext('2d')) &&
+           elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+  })();
+
   const objects = document.getElementsByClassName('async_home');
+
   Array.from(objects).map((item) => {
     // Start loading image
-    const img = new Image();
+    let imageUrl;
     if (window.matchMedia("(max-width: 575.98px)").matches) {
       /*img.src = item.dataset.mobile;
       img.onload = () => {
@@ -154,24 +163,29 @@ $('.no-img').css({ 'height': cw + 'px' });
       }; */
     } else if (window.matchMedia("(max-width: 970px)").matches) {
       // Once image is loaded replace the src of the HTML element
-      img.src = item.dataset.tablet;
-      img.onload = () => {
-        item.classList.remove('async_home');
-        return item.nodeName === 'IMG' ?
-          item.src = item.dataset.src :
-          item.style.backgroundImage = `linear-gradient(130deg, rgba(0, 183, 148, 0.7) 0.65%, rgba(36, 107, 150, 0.7) 112%), url(${item.dataset.tablet})`
-      };
+      imageURL = item.dataset.tablet;
     } else {
       // Once image is loaded replace the src of the HTML element
-      img.src = item.dataset.src;
-      img.onload = () => {
-        item.classList.remove('async_home');
-        return item.nodeName === 'IMG' ?
-          item.src = item.dataset.src :
-          item.style.backgroundImage = `linear-gradient(130deg, rgba(0, 183, 148, 0.7) 0.65%, rgba(36, 107, 150, 0.7) 112%), url(${item.dataset.src})`
-      };
+      imageURL = item.dataset.src;
     }
 
+    // Use .webp if supported
+    if (supportsWebP) {
+      imageURL = imageURL.replace(/\.(jpg|jpeg|png)$/i, '.webp');
+    }
+
+    const img = new Image();
+    img.src = imageURL;
+
+    img.onload = () => {
+      item.classList.remove('async_home');
+
+      if (item.nodeName === 'IMG') {
+        item.src = imageURL;
+      } else {
+        item.style.backgroundImage = `linear-gradient(130deg, rgba(0, 183, 148, 0.7) 0.65%, rgba(36, 107, 150, 0.7) 112%), url(${imageURL})`;
+      }
+    };
   });
 })();
 
