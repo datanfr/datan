75133f31e12feae6745ea70b5f0dca832995fa3e
Awenig Marié <awenigmar@gmail.com>
2025-07-24 13:27:44 +0200

    Change function for 4 width sizes

--- application/libraries/Image_service.php
+++ application/libraries/Image_service.php
@@ -2,7 +2,7 @@
 
 class Image_service
 {
-    public function convert_to_webp_with_imagick($source_path, $destination_path, $quality = 80)
+    public function convert_to_webp($source_path, $destination_path, $quality = 80)
     {
         try {
             $image = new \Imagick($source_path);
