ce61a38e52ef6eedfa5589501954efbcee01117f
Awenig Marié <awenigmar@gmail.com>
2026-02-05 12:27:12 +0100

    Add municipales to homepage

--- application/views/home/partials/election.php
+++ application/views/home/partials/election.php
@@ -3,14 +3,14 @@
   <div class="container p-md-0">
     <div class="row py-4">
       <div class="col-12">
-        <h2 class="text-center my-4"><span class="text-primary">Élections législatives 2022</span><span style="display: block" class="mt-3">Découvrez les députés réélus</span></h2>
+        <h2 class="text-center my-4">Élections municipales 2026</h2>
+        <h3 class="mt-3">Découvrez les députés candidats aux municipales</h3>
       </div>
     </div>
     <div class="row pt-2 pb-5">
       <div class="col-md-7 d-flex flex-column justify-content-center">
-        <p>Les élections législatives prochaines se sont déroulées les dimanches <b>12 et 19 juin 2022</b>.</p>
-        <p>Aux élections législatives, <?= $candidatsN ?> députés étaient candidats à leur réélection.</p>
-        <p>Au total, <span class="font-weight-bold text-primary"><?= $electedN ?> députés</span> ont été réélus pendant les législatives de 2022.</p>
+        <p>Les élections municipales se dérouleront les dimanches <b>15 et 22 mars 2026</b>.</p>
+        <p>Notre équipe a répertorié <?= $candidatsN ?> député<?= $candidatsN > 1 ? "s" : "" ?> candidat<?= $candidatsN > 1 ? "s" : "" ?> aux municipales de 2026.</p>
       </div>
       <div class="col-md-5 mt-5 mt-md-0">
         <div class="d-flex justify-content-center">
@@ -21,7 +21,7 @@
     <div class="row pb-5">
       <div class="col-12 d-flex justify-content-center">
         <a href="<?= base_url();?>elections/legislatives-2022" class="no-decoration">
-          <button type="button" class="btn btn-primary">Découvrez les députés réélus</button>
+          <button type="button" class="btn btn-primary">Découvrez les députés candidats</button>
         </a>
       </div>
     </div>
