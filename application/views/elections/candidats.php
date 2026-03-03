01e7521621b53e168acd17cb4180305c87571d3c
Awenig Marié <awenigmar@gmail.com>
2026-03-03 15:19:18 +0100

    Improve search (icon)

--- application/views/elections/candidats.php
+++ application/views/elections/candidats.php
@@ -33,15 +33,15 @@
       <div class="row">
         <div class="col-12">
           <div class="city-search-block">
-            <h2 class="mb-1">Recherchez votre commune</h2>
-            <p class="text-muted mb-3" style="font-size: 0.9rem;">Accédez aux listes candidates et aux résultats dans votre commune</p>
-          <div class="city-search-wrapper d-flex">
+            <h2 class="mb-1">Candidats et résultats dans votre commune</h2>
+            <p class="text-muted mb-3">Accédez aux listes candidates et aux résultats dans votre commune</p>
+            <div class="city-search-wrapper d-flex">
               <div class="city-search-input-wrapper flex-grow-1">
-                  <svg class="city-search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.099zm-5.242 1.656a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
-                  </svg>
-                  <input type="text" id="citySearch" class="city-search-input" placeholder="Nom de commune (ex: Rennes, Lyon, Bordeaux...)">
-              </div>
+                <div class="city-search-icon">
+                  <?= file_get_contents(FCPATH . "assets/imgs/icons/bi-search.svg") ?>
+                </div>
+              <input type="text" id="citySearch" class="city-search-input" placeholder="Nom de commune (ex: Rennes, Lyon, Bordeaux...)">
+            </div>
           </div>
         </div>
         <!-- TOP 10 CITIES -->
