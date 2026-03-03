6318d8af2451b40c33f4648e4875ab9fc4a1dbaf
Awenig Marié <awenigmar@gmail.com>
2026-03-03 15:12:47 +0100

    Improve pg election

--- application/models/Elections_model.php
+++ application/models/Elections_model.php
@@ -52,45 +52,38 @@
     public function get_election_infos($type){
       if ($type == "Régionales") {
         $info = "
-        <p><b>Les élections régionales ont lieu tous les six ans</b>, en même temps que les élections départementales.</p>
-        <p>Les élections régionales permettent d'<b>élire les conseillers régionaux</b> qui composent le conseil régional. Ce sont les conseillers régionaux qui décident et votent sur les affaires de la région.</p>
-        <p><b>Les compétences des régions sont multiples</b>. Il s'agit par exemple du développement économique et social, de l'organisation du transport, de la construction des lycées, ou encore de l'organisation de la formation professionnelle.</p>
-        <p>Les candidats se rassemblent sur des listes politiques, en fonction de leur parti d'appartenance. Le jour de l'élection, les électeurs votent donc pour une liste.</p>
-        <p><b>L'élection est organisée en deux tours</b>. Au premier tour, si aucune liste obtient plus de 50% des suffrages exprimés, un second tour est organisé avec uniquement les listes ayant récolté plus de 10% des voix.</p>
-        <p><b>La liste arrivant en tête se voit attribuer 25% des sièges</b> du conseil régional. Pour les sièges restants, ils sont attribués à la proportionnelle entre toutes les listes ayant obtenu au moins 5% des voix.</p>
+        <p>Les élections régionales ont lieu <b>tous les six ans</b>, en même temps que les départementales. Elles permettent d'élire les <b>conseillers régionaux</b> qui composent le conseil régional.</p>
+        <p>Les compétences des régions incluent le développement économique, les transports, la construction des lycées et la formation professionnelle.</p>
+        <p>Les électeurs votent pour une <b>liste politique</b>. L'élection se déroule en <b>deux tours</b> : si aucune liste n'obtient plus de 50% au premier tour, un second tour est organisé entre les listes ayant recueilli au moins <b>10%</b> des suffrages.</p>
+        <p>La liste arrivée en tête reçoit <b>25% des sièges</b>, le reste étant réparti à la proportionnelle entre toutes les listes ayant obtenu au moins <b>5%</b>.</p>
         ";
       } elseif ($type == "Départementales") {
         $info = "
-        <p><b>Les élections départementales ont lieu tous les six ans</b>, en même temps que les élections régionales.</p>
-        <p>Les élections départementales permettent d'<b>élire les conseillers départementaux</b> qui composent le conseil régional. Ce sont les conseillers départementaux qui décident et votent sur les affaires de la région. Avant 2013, les conseillers départementaux étaient appelés conseillers généraux.</p>
-        <p><b>Les compétences des régions sont multiples</b>. La compétence la plus importante est sociale. Les conseils départementaux gèrent l'aide sociale à l'enfant, les politiques d'hebergement et d'insertion des personnes handicapées, ou encore la gestion de maisons de retraite. Les conseils départementaux ont également des compétences en matière d'éducation (construction et entretien de collèges), la voirie départementale, ou encore l'action culturelle et sportive.</p>
-        <p>Les candidats doivent former un <b>binôme mixte</b> (une candidat femme et un candidat homme). Le jours de l'élection, les électeurs votent pour un binôme candidat.</p>
-        <p><b>L'élection est organisée en deux tours</b>. Au premier tour, si un binôme obtient la majorité absolue des voix, alors il est automatiquement élu. Si aucun binôme ne l'emporte, un second tour est organisé.</p>
-        <p>Les binômes ayant obtenu au moins 12,5% des voix des électeurs inscrits peuvent se présenter au second tour. <b>Au second tour, le binôme obtenant le plus de voix l'emporte</b>.
+        <p>Les élections départementales ont lieu <b>tous les six ans</b>, en même temps que les régionales. Elles permettent d'élire les <b>conseillers départementaux</b> qui composent le conseil départemental.</p>
+        <p>Les conseils départementaux ont des compétences variées : action sociale (aide à l'enfance, handicap, maisons de retraite), éducation (construction et entretien des collèges), voirie départementale et action culturelle.</p>
+        <p>Les candidats se présentent en <b>binôme mixte</b> (un homme, une femme). L'élection se déroule en <b>deux tours</b> : un binôme est élu au premier tour s'il obtient la majorité absolue, sinon un second tour est organisé entre les binômes ayant recueilli au moins <b>12,5% des inscrits</b>. Le binôme arrivé en tête l'emporte.</p>
         ";
       } elseif ($type == "Législatives") {
         $info = "
-        <p>Les élections législatives ont lieu <b>tous les cinq ans</b>, quelques semaines après l'élection présidentielle.</p>
-        <p>Les élections législatives permettent <b>d'élire les 577 députés</b> qui composent l'Assemblée nationale.</p>
-        <p><b>Comment les députés sont-ils élus ?</b> Lors du premier tour, un candidat peut être élu s'il obtient la majorité des suffrages exprimés (plus de 50%). Si aucun candidat ne remplit ces conditions, un second tour est organisé. Les candidats ayant reçu un nombre de suffrages supérieur à 12,5% des inscrits peuvent se présenter au second tour. Le vainqueur est le candidat qui arrive en tête au second tour.</p>
-        <p>Les rôles du député sont multiples. Ils votent la loi, contrôlent le gouvernement, et représentent les intérêts de leur circonscription au niveau national. Pour plus d'informations, <a href='https://datan.fr/faq' target='_blank'>cliquez ici</a>.</p>
+        <p>Les élections législatives ont lieu <b>tous les cinq ans</b>, quelques semaines après la présidentielle. Elles permettent d'élire les <b>577 députés</b> de l'Assemblée nationale.</p>
+        <p>L'élection se déroule en <b>deux tours</b> : un candidat est élu au premier tour s'il obtient la majorité absolue, sinon un second tour est organisé entre les candidats ayant recueilli au moins <b>12,5% des inscrits</b>. Le candidat arrivé en tête est élu.</p>
+        <p>Les députés votent la loi, contrôlent le gouvernement et représentent leur circonscription. <a href='https://datan.fr/faq' target='_blank'>En savoir plus</a>.</p>
         ";
       } elseif ($type == "Présidentielle") {
         $info = "
-        <p>L'élection présidentielle a lieu <b>tous les cinq ans</b>, quelques semaines avant les élections législatives. Elle permet d'élire le président de la République.</p>
-        <p><b>Comment le président est-il élu ?</b> Pour être candidat, il faut avoir récolté le parrainage de 500 élus (maires, parlementaires, conseillers régionaux, etc.) L'élection se déroule sur deux tours. Lors du premier tour, un candidat peut être élu s'il obtient la majorité des suffrages exprimés (plus de 50%). Si aucun candidat n'y parvient, un second tour est organisé avec les deux candidats arrivés en tête au premier tour. Le vainqueur est le candidat qui arrive en tête du second tour.</p>
-        <p>Les rôles du président de la République sont nombreux. Il ou elle est veille au respect de la Constitution, assure le fonctionnement des institutions, nomme le Premier ministre et préside le Conseil des ministres, promulgue les lois, etc. Pour plus d'informations, <a href='https://www.vie-publique.fr/infographie/23816-infographie-quel-est-le-role-du-president-de-la-republique' target='_blank' rel='nofollow noreferrer noopener'>cliquez ici</a>.</p>
+        <p>L'élection présidentielle a lieu <b>tous les cinq ans</b>, quelques semaines avant les législatives. Elle permet d'élire le président de la République.</p>
+        <p>Pour être candidat, il faut obtenir le parrainage de <b>500 élus</b>. L'élection se déroule en <b>deux tours</b> : un candidat est élu au premier tour s'il obtient la majorité absolue, sinon un second tour est organisé entre les deux candidats arrivés en tête.</p>
+        <p>Le président veille au respect de la Constitution, nomme le Premier ministre, préside le Conseil des ministres et promulgue les lois. <a href='https://www.vie-publique.fr/infographie/23816-infographie-quel-est-le-role-du-president-de-la-republique' target='_blank' rel='nofollow noreferrer noopener'>En savoir plus</a></p>
         ";
       } elseif ($type == "Européennes") { 
         $info = "
-        <p>Les élections européennes ont lieu <b>tous les cinq ans</b>. Elles permettent d'élire les députés européens, qui ont pour rôle d'élire la Commission européenne et d'amender et voter les lois européennes (règlements et directives).</p>
-        <p>En France, les députés européens sont élus au scrutin proportionnel. Les listes obtenant au moins 5% des suffrages exprimés se voient attribuer des sièges, en fonction de leur score électoral.</p>
+        <p>Les élections européennes ont lieu <b>tous les cinq ans</b> et permettent d'élire les députés européens, chargés d'élire la Commission européenne et de voter les lois européennes (règlements et directives).</p>
+        <p>En France, l'élection se fait au <b>scrutin proportionnel de liste</b>. Les listes obtenant au moins <b>5%</b> des suffrages se voient attribuer des sièges proportionnellement à leur score.</p>
         ";
       } elseif($type == "Municipales") {
         $info = "
-        <p>Les élections municipales ont lieu <b>tous les six ans</b> et permettent d'élire les conseillers municipaux qui composent le conseil municipal de chaque commune, ainsi que le maire et ses adjoints. Le nombre de conseillers municipaux varie de 7 à 69 en fonction de la taille des communes.</p>
-        <p>Les conseillers municipaux sont élus via un <b>scrutin proportionnel de liste</b>. Au premier tour, si une liste obtient la majorité absolue, elle remporte la moitié des sièges, le reste étant réparti proportionnellement entre toutes les listes ayant obtenu au moins 5% des suffrages. Si aucune liste n'obtient la majorité absolue, un second tour est organisé entre les listes ayant receuilli au moins 10% des suffrages.</p>
-        <p>Le conseil municipal élit ensuite le maire.</p>
+        <p>Les élections municipales ont lieu <b>tous les six ans</b> et permettent d'élire les conseillers municipaux de chaque commune, ainsi que le maire et ses adjoints. Le nombre de conseillers varie de 7 à 69 selon la taille de la commune.</p>
+        <p>L'élection se fait au <b>scrutin proportionnel de liste</b>. Si une liste obtient la majorité absolue au premier tour, elle remporte la moitié des sièges, le reste étant réparti entre les listes ayant obtenu au moins 5%. Sinon, un second tour est organisé entre les listes ayant recueilli au moins 10% des suffrages. Le conseil municipal élit ensuite le maire.</p>
         ";
       } else {
         $info = NULL;
--- application/views/elections/candidats.php
+++ application/views/elections/candidats.php
@@ -1,80 +1,69 @@
 <div class="container pg-elections-candidats">
   <div class="row bloc-titre">
-    <div class="col-12">
+    <div class="col-10">
       <h1><?= $title ?></h1>
-    </div>
-  </div>
-</div>
-<div class="container-fluid pg-elections-candidats infosIndividual py-5">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-lg-7 my-4">
-        <h2>Informations</h2>
-        <p>Les <?= mb_strtolower($election['libelle']) ?> <?= $election['dateYear'] ?> se déroulent en <?= $election['dateSecondRound'] ? "deux tours" : "un tour" ?>.</p>
-        <?php if ($election['dateSecondRound']): ?>
-          <p>Le premier tour <?= $today > $election['dateFirstRound'] ? "s'est tenu" : "se tiendra" ?> le <?= $election['dateFirstRoundFr'] ?>, tandis que le second tour <?= $today > $election['dateSecondRound'] ? "s'est déroulé" : "se déroulera" ?> le <?= $election['dateSecondRoundFr'] ?>.</p>
-        <?php else: ?>
-          <p>L'élection <?= $today > $election['dateFirstRound'] ? "s'est tenue" : "se tiendra" ?> le <?= $election['dateFirstRoundFr'] ?>.</p>
-        <?php endif; ?>
-        <?php if ($election['candidates']): ?>
-          <p>
-            Découvrez sur cette page les députés candidats aux <?= mb_strtolower($election['libelle']) ?> de <?= $election['dateYear'] ?>.
-          </p>
-          <p>
-            <?php if ($candidatsN): ?>
-              Nous avons répertorié <b><?= $candidatsN ?> député<?= $candidatsN > 1 ? "s" : NULL ?> candidat<?= $candidatsN > 1 ? "s" : NULL ?></b><?php if (isset($candidatsNLeaders) && $candidatsNLeaders > 0): ?>, dont <?= $candidatsNLeaders ?> têtes de liste<?php endif; ?>.
-            <?php else: ?>
-              Nous avons jusqu'à présent répertorié <span class="font-weight-bold">aucun député candidat</span>.
-            <?php endif; ?>
-          </p>
-          <?php if ($state == 1): ?>
-            <p>
-              De ces candidats, <b><?= $candidatsN_second ?> député<?= $candidatsN_second > 1 ? "s se sont maintenus" : " s'est maintenu" ?> pour le second tour</b>.
-            </p>
+      <div class="mt-5">
+        <p>
+          Découvrez les résultats et candidats aux <?= mb_strtolower($election['libelle']) ?> <?= $election['dateYear'] ?>. Ces élections se déroulent en <?= $election['dateSecondRound'] ? "deux tours" : "un tour" ?>.
+          <?php if ($election['dateSecondRound']): ?>
+            Le premier tour <?= $today > $election['dateFirstRound'] ? "s'est tenu" : "se tiendra" ?> le <?= $election['dateFirstRoundFr'] ?>, tandis que le second tour <?= $today > $election['dateSecondRound'] ? "s'est déroulé" : "se déroulera" ?> le <?= $election['dateSecondRoundFr'] ?>.
+          <?php else: ?>
+            L'élection <?= $today > $election['dateFirstRound'] ? "s'est tenue" : "se tiendra" ?> le <?= $election['dateFirstRoundFr'] ?>.
           <?php endif; ?>
-          <?php if ($state == 2): ?>
-            <p>Au total, <b><?= $candidatsN_elected ?></b> députés en exercice ont été élus lors des <?= mb_strtolower($election['libelle']) ?> de <?= $election['dateYear'] ?>.</p>
-          <?php endif; ?>
-          </p>
+          Découvrez les résultats des élections commune par commune sur Datan.
+        </p>
+        <?php if($election['slug'] == 'municipales-2026'): ?>
+          <div class="alert alert-primary mt-4" role="alert">
+            Le premier tour des élections municipales se tiendra le dimanche 15 mars 2026. Les résultats seront diffusés le lendemain sur Datan.
+          </div>
         <?php endif; ?>
-      </div>
-      <div class="col-md-4 col-lg-5 d-flex justify-content-center align-items-center">
-        <?php if (!empty($election['resultsUrl'])): ?>
-          <span class="url_obf btn btn-light btn-lg" url_obf="<?= url_obfuscation($election['resultsUrl']) ?>">
-            Résultats de l'élection
-          </span>
-        <?php endif; ?>
+        <div class="mt-5 infosGeneral">
+          <h2 class="title ml-md-5 ml-3">Mieux comprendre</h2>
+          <div class="px-4">
+            <?= $electionInfos ?>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
+<?php if($election['slug'] == 'municipales-2026'): ?>
+  <div class="container-fluid pg-elections-candidats search py-5 mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="city-search-block">
+            <h2 class="mb-1">Recherchez votre commune</h2>
+            <p class="text-muted mb-3" style="font-size: 0.9rem;">Accédez aux listes candidates et aux résultats dans votre commune</p>
+          <div class="city-search-wrapper d-flex">
+              <div class="city-search-input-wrapper flex-grow-1">
+                  <svg class="city-search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.099zm-5.242 1.656a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/>
+                  </svg>
+                  <input type="text" id="citySearch" class="city-search-input" placeholder="Nom de commune (ex: Rennes, Lyon, Bordeaux...)">
+              </div>
+          </div>
+        </div>
+        <!-- TOP 10 CITIES -->
+        <div class="top-cities mt-5">
+          <p class="top-cities-label">Grandes villes</p>
+          <div class="d-flex flex-wrap">
+            <?php foreach(array_slice($communes, 0, 15) as $commune): ?>
+              <a href="<?= base_url() ?>elections/resultats/<?= $commune['slug'] ?>/<?= $commune['commune_slug'] ?>" class="top-city-pill"><?= $commune['commune_nom'] ?></a>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
 <div class="container pg-elections-candidats mt-4 mb-5">
   <div class="row">
     <?php if ($results): ?>
-      <div class="col-md-8 col-lg-7">
+      <div class="col-10">
         <?php $this->load->view('elections/results/'.$election['slug'].'.php') ?>
       </div>
-      <?php if (!empty($electionInfos)): ?>
-        <div class="col-md-8 col-lg-4 offset-lg-1 mt-5 mt-lg-0">
-          <div class="mt-4 infosGeneral">
-            <h2 class="title ml-md-5 ml-3">Mieux comprendre</h2>
-            <div class="px-4">
-              <?= $electionInfos ?>
-            </div>
-          </div>
-        </div>
-      <?php endif; ?>
-    <?php else: ?>
-      <?php if (!empty($electionInfos)): ?>
-        <div class="col-12">
-          <div class="mt-4 infosGeneral">
-            <h2 class="title ml-md-5 ml-3">Mieux comprendre</h2>
-            <div class="px-4">
-              <?= $electionInfos ?>
-            </div>
-          </div>
-        </div>
-      <?php endif; ?>
     <?php endif; ?>
   </div>
   <?php if ($election['candidates']): ?>
