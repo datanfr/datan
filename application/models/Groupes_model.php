e5b363bf8cc45ffaccb65a66f194ce5c1f4d54ba
Awenig Marié <awenigmar@gmail.com>
2026-04-16 10:07:38 +0200

    Fix bug coalition tool

--- application/models/Groupes_model.php
+++ application/models/Groupes_model.php
@@ -53,7 +53,7 @@
       $this->db->select('o.libelleAbrev, o.libelle, e.effectif AS seats, o.couleurAssociee');
       $this->db->order_by('effectif', 'desc');
       $this->db->join('organes o', 'o.uid = e.organeRef', 'left');
-      $this->db->where('e.legislature', legislature_current());
+      $this->db->where('o.dateFin', NULL);
       $groupes = $this->db->get('groupes_effectif e')->result_array();
 
       foreach ($groupes as $key => $value) {
